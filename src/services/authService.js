96a21e67c3937446f6146bbc0357dfe438e41f54
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-22 04:03:39 +0200

    Add: gatby's auth service code for checking user, setting and getting user and logout

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,9 +2,41 @@ import http from './httpService';
 
 const apiEndpoint = `${process.env.API_URL}/auth/jwt/create`;
 
-export function login(email, password) {
-  return http.post(apiEndpoint, {
+/*  Initial auth service from Gatsby's tutorial:
+ *  https://www.gatsbyjs.com/tutorial/authentication-tutorial/
+ */
+export const isBrowser = () => typeof window !== 'undefined';
+
+export const getUser = () =>
+  isBrowser() && window.localStorage.getItem('cotalUser')
+    ? JSON.parse(window.localStorage.getItem('cotalUser'))
+    : {};
+
+const setUser = (user) =>
+  window.localStorage.setItem('cotalUser', JSON.stringify(user));
+
+export const handleLogin = ({ email, password }) => {
+  const jsonToken = http.post(apiEndpoint, {
     email: email,
     password: password,
   });
-}
+
+  if (jsonToken.statusCode === 'ok') {
+    return setUser({
+      jsonToken,
+    });
+  }
+
+  return false;
+};
+
+export const isLoggedIn = () => {
+  const user = getUser();
+
+  return !!user.jsonToken;
+};
+
+export const logout = (callback) => {
+  setUser({});
+  callback();
+};
