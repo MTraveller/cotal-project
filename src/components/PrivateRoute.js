05df9b15d464a2191bb2c520f6f203ae0a39789d
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-22 03:11:05 +0200

    Add: Gatsby's private route component

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { navigate } from 'gatsby';
+import { isLoggedIn } from '../services/auth';
+
+// Private route from Gatsby's
+// private route example, source:
+// https://www.gatsbyjs.com/docs/how-to/routing/client-only-routes-and-user-authentication/
+const PrivateRoute = ({ component: Component, location, ...rest }) => {
+  if (!isLoggedIn() && location.pathname !== `/`) {
+    navigate('/');
+    return null;
+  }
+
+  return <Component {...rest} />;
+};
+
+export default PrivateRoute;
