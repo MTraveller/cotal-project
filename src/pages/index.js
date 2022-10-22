70d9d30b66809774bbbbf3a2db6631b86162359f
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-22 03:02:27 +0200

    Remove: useState import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Seo from '../components/seo';
 import LoginForm from '../components/LoginForm';
