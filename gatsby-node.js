8958c8376fd7f25f1fc877571a1fb3d487c77ffb
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-22 18:42:43 +0200

    Add: gatsby node onCreatePage for profile based urls

---
+++ gatsby-node.js
@@ -0,0 +1,15 @@
+// Tutorial from: https://www.gatsbyjs.com/tutorial/authentication-tutorial/#creating-client-only-routes
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions;
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/profile/)) {
+    page.matchPath = '/profile/*';
+
+    // Update the page.
+    createPage(page);
+  }
+};
