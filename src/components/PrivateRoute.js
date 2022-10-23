793757102007188b3c910faa2a4797508f657f47
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-23 13:58:53 +0200

    Remove: console.log()

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -8,7 +8,6 @@ import { isLoggedIn } from '../services/authService';
 const PrivateRoute = ({ component: Component, location, ...rest }) => {
   if (!isLoggedIn() && location.pathname !== `/`) {
     navigate('/');
-    console.log('NOT LOGGED IN!');
     return null;
   }
 
