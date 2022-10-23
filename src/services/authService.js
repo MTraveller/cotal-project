9d06842b355999e9da0ac37ba5b85adb219d9488
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-24 00:43:09 +0200

    Refactor: convert async await to promise -> catch -> then -> finally to solve promise in return in PrivateRoute component isLoggedIn

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,17 +2,18 @@ import http from './httpService';
 
 const apiLoginEndpoint = `${process.env.API_URL}/auth/jwt/create/`;
 const apiRefreshEndpoint = `${process.env.API_URL}/auth/jwt/refresh/`;
-const apiTestMeEndpoint = `${process.env.API_URL}/profiles/me/`;
+const apiTestTokenEndpoint = `${process.env.API_URL}/`;
 
 /*  Initial auth service from Gatsby's tutorial:
  *  https://www.gatsbyjs.com/tutorial/authentication-tutorial/
  */
 export const isBrowser = () => typeof window !== 'undefined';
 
-export const getUser = () =>
-  isBrowser() && window.localStorage.getItem('cotalUser')
+export const getUser = () => {
+  return isBrowser() && window.localStorage.getItem('cotalUser')
     ? JSON.parse(window.localStorage.getItem('cotalUser'))
     : {};
+};
 
 const setUser = (user) =>
   window.localStorage.setItem('cotalUser', JSON.stringify(user));
@@ -34,27 +35,30 @@ export const handleLogin = async (email, password) => {
   return false;
 };
 
-export const isLoggedIn = async () => {
-  const { user } = getUser();
-  if (!user.access) return false;
+export const isLoggedIn = () => {
+  let user = getUser();
+  if (!user?.access) return !user?.access;
 
-  let res = '';
-  try {
-    res = await http.get(apiTestMeEndpoint, {
+  http
+    .get(apiTestTokenEndpoint, {
       headers: {
         Authorization: `JWT ${user.access}`,
       },
+    })
+    .catch((ex) => {
+      if (ex.response.status === 401) {
+        http
+          .post(apiRefreshEndpoint, {
+            refresh: user.refresh,
+          })
+          .then((res) => {
+            user['access'] = res.data.access;
+          })
+          .finally(() => {
+            setUser(user);
+          });
+      }
     });
-  } catch (ex) {
-    if (user.access && ex.response.data.messages[0].message) {
-      res = await http.post(apiRefreshEndpoint, {
-        refresh: user.refresh,
-      });
-
-      user['access'] = res.data.access;
-      setUser({ user });
-    }
-  }
 
   return !!user.access;
 };
