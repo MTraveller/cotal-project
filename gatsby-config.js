fad35eab63e1b85880c234724530fa18f7c235af
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-26 05:30:14 +0200

    Add: gatsby layout plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
         icon: `src/images/fff.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-layout`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-netlify`,
   ],
