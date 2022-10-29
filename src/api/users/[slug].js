4b130bc7a4e5d469e54bf9d659131989e2738593
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-29 14:55:50 +0200

    Refactor: slug SSR in pages to headless function

---
+++ src/api/users/[slug].js
@@ -0,0 +1,14 @@
+import http from '../../services/httpService';
+
+export default async function getUserHandler(req, res) {
+  // GET user from api
+  const slug = req.params['slug'];
+  const url = `${process.env.API_URL}/profiles/${slug}/`;
+
+  try {
+    const result = await http.get(url);
+    res.send(result.data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
