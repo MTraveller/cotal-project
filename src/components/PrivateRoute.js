253709861b617a77938bc76513d849a8c72a99fc
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-29 15:07:45 +0200

    Add: previousPath from gatsby's globalHistory

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { navigate } from 'gatsby';
 import { toast } from 'react-toastify';
+import { globalHistory } from '@gatsbyjs/reach-router/lib/history';
+
 import { isLoggedIn } from '../services/authService';
 
 // Private route from Gatsby's
 // private route example, source:
 // https://www.gatsbyjs.com/docs/how-to/routing/client-only-routes-and-user-authentication/
 const PrivateRoute = ({ component: Component, location, ...rest }) => {
-  if (!isLoggedIn() && location.pathname !== `/`) {
+  const previousPath = globalHistory.location.pathname;
+
+  if (!isLoggedIn() && previousPath !== `/` && location.pathname !== `/`) {
     navigate(`/`);
     toast(`Please login, to continue.`);
     return null;
