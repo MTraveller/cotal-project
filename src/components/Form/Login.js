d0f70f863cff033ca00795198871d5975c14ba55
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-30 21:06:48 +0100

    Refactor: padding for email and password inputs & button

--- src/components/Form/Login.js
+++ src/components/Form/Login.js
@@ -59,7 +59,7 @@ const Login = () => {
             value={account.email}
             autoComplete="email"
             required
-            className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-3 md:py-4 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             placeholder="Email address"
             onChange={handleChange}
           />
@@ -75,7 +75,7 @@ const Login = () => {
             value={account.password}
             autoComplete="current-password"
             required
-            className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-3 md:py-4 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             placeholder="Password"
             onChange={handleChange}
           />
@@ -94,7 +94,7 @@ const Login = () => {
       <div>
         <button
           type="submit"
-          className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
         >
           <span className="absolute inset-y-0 left-0 flex items-center pl-3">
             <svg
