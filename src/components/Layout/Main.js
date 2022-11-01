f3a4d0cf7eb5754ea451826c0fcd49000f5cc560
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-01 15:29:58 +0100

    Refactor: add styling to frontpage main html tag

--- src/components/Layout/Main.js
+++ src/components/Layout/Main.js
@@ -5,7 +5,9 @@ const MainStyles = styled.main``;
 
 const Main = ({ children, location }) => {
   return location.pathname === `/` ? (
-    <main>{children}</main>
+    <main className="w-full h-full flex flex-wrap content-center justify-center gap-y-14 lg:gap-y-0 bg-stone-900 dark:bg-neutral-900">
+      {children}
+    </main>
   ) : (
     <MainStyles>{children}</MainStyles>
   );
