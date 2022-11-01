c28b94569fa5e491b900d27c68fe112916bad094
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-01 19:46:23 +0100

    Refactor: move submit button to dedicated component to reuse on multiple forms

--- src/components/Form/Login.js
+++ src/components/Form/Login.js
@@ -8,6 +8,7 @@ import {
   TopLoginRegisterInput as EmailInput,
   BottomLoginRegisterInput as PasswordInput,
 } from './input/indexInput';
+import { SubmitButton } from './indexButton';
 
 const Login = ({ form }) => {
   const previousPath = globalHistory.location.pathname;
@@ -91,14 +92,14 @@ const Login = ({ form }) => {
             className="font-medium text-slate-100 hover:text-slate-300"
             onClick={toggleForm}
           >
-            Sign-up
+            New to Cotal?
           </button>
         </div>
 
         <div className="text-sm">
           <a
             href="#"
-            className="font-medium text-indigo-600 hover:text-indigo-500"
+            className="font-medium text-slate-100 hover:text-slate-300"
           >
             Forgot your password?
           </a>
@@ -106,27 +107,7 @@ const Login = ({ form }) => {
       </div>
 
       <div>
-        <button
-          type="submit"
-          className="group relative flex w-full justify-center rounded-md border border-transparent bg-slate-300 py-3 px-4 text-sm font-medium text-slate-500 hover:text-white hover:bg-lime-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2"
-        >
-          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-            <svg
-              className="h-5 w-5 text-slate-500 group-hover:text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </span>
-          Sign in
-        </button>
+        <SubmitButton buttonText="Sign in" />
       </div>
     </form>
   );
