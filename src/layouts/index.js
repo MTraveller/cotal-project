94541f26c88eac37c3d33db4938fff0fc0d5890b
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-02 11:28:34 +0100

    Add: gradient background below width of 1023px

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { useStaticQuery, graphql } from 'gatsby';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
@@ -9,6 +10,29 @@ import UserContextProvider, { UserContext } from '../context/UserContext';
 import Header from '../components/Layout/Header';
 import Main from '../components/Layout/Main';
 import Footer from '../components/Layout/Footer';
+
+const IndexDivStyles = styled.div`
+  height: 100%;
+
+  @media all and (max-width: 1023px) {
+    background-image: linear-gradient(
+      45deg,
+      hsl(0deg 0% 9%) 0%,
+      hsl(240deg 7% 14%) 1%,
+      hsl(244deg 16% 18%) 4%,
+      hsl(242deg 23% 22%) 9%,
+      hsl(241deg 31% 26%) 18%,
+      hsl(242deg 39% 30%) 32%,
+      hsl(241deg 47% 35%) 49%,
+      hsl(238deg 41% 35%) 68%,
+      hsl(234deg 36% 37%) 82%,
+      hsl(229deg 31% 38%) 91%,
+      hsl(225deg 26% 39%) 96%,
+      hsl(221deg 21% 40%) 99%,
+      hsl(218deg 15% 41%) 100%
+    );
+  }
+`;
 
 const Layout = ({ children, location }) => {
   const data = useStaticQuery(graphql`
@@ -27,7 +51,7 @@ const Layout = ({ children, location }) => {
       <UserContextProvider>
         <UserContext.Consumer>
           {(value) => (
-            <>
+            <IndexDivStyles>
               <Header
                 location={location}
                 isLoggedIn={value.isLoggedIn}
@@ -38,11 +62,11 @@ const Layout = ({ children, location }) => {
                 isLoggedIn={value.isLoggedIn}
                 children={children}
               />
-            </>
+              <Footer location={location} />
+            </IndexDivStyles>
           )}
         </UserContext.Consumer>
       </UserContextProvider>
-      <Footer location={location} />
     </ClientOnly>
   );
 };
