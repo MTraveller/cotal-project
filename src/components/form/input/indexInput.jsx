0f4c8aaa2290f3926912d5889f7de3ea3776b46e
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-02 14:50:12 +0100

    Add: input controls to use parent account state

--- src/components/form/input/indexInput.jsx
+++ src/components/form/input/indexInput.jsx
@@ -1,40 +1,67 @@
 import React from 'react';
 
-export const TopLoginRegisterInput = ({ id, name, type, placeholder }) => {
+export const TopLoginRegisterInput = ({
+  id,
+  name,
+  type,
+  value,
+  placeholder,
+  onChange,
+}) => {
   return (
     <input
       id={id}
       name={name}
       type={type}
+      value={value}
       required
       className="relative block w-full appearance-none rounded-none rounded-t-md border border-slate-400 bg-slate-500 px-3 py-3 md:py-4 text-white placeholder-white focus:z-10 focus:border-yellow-300 focus:outline-none focus:ring-yellow-300 sm:text-sm"
       placeholder={placeholder}
+      onChange={onChange}
     />
   );
 };
 
-export const MiddleLoginRegisterInput = ({ id, name, type, placeholder }) => {
+export const MiddleLoginRegisterInput = ({
+  id,
+  name,
+  type,
+  value,
+  placeholder,
+  onChange,
+}) => {
   return (
     <input
       id={id}
       name={name}
       type={type}
+      value={value}
       required
       className="relative block w-full appearance-none rounded-none border border-slate-400 bg-slate-500 px-3 py-3 md:py-4 text-white placeholder-white focus:z-10 focus:border-yellow-300 focus:outline-none focus:ring-yellow-300 sm:text-sm"
       placeholder={placeholder}
+      onChange={onChange}
     />
   );
 };
 
-export const BottomLoginRegisterInput = ({ id, name, type, placeholder }) => {
+export const BottomLoginRegisterInput = ({
+  id,
+  name,
+  type,
+  value,
+  placeholder,
+  onChange,
+}) => {
   return (
     <input
       id={id}
       name={name}
       type={type}
+      value={value}
       required
       className="relative block w-full appearance-none rounded-none rounded-b-md border border-slate-400 bg-slate-500 px-3 py-3 md:py-4 text-white placeholder-white focus:z-10 focus:border-yellow-300 focus:outline-none focus:ring-yellow-300 sm:text-sm"
       placeholder={placeholder}
+      onChange={onChange}
     />
   );
 };
@@ -43,17 +70,21 @@ export const NameLoginRegisterInput = ({
   id,
   name,
   type,
-  placeholder,
+  value,
   borderRadius,
+  placeholder,
+  onChange,
 }) => {
   return (
     <input
       id={id}
       name={name}
       type={type}
+      value={value}
       required
       className={`relative block w-full appearance-none rounded-none ${borderRadius} border border-slate-400 bg-slate-500 px-3 py-3 md:py-4 text-white placeholder-white focus:z-10 focus:border-yellow-300 focus:outline-none focus:ring-yellow-300 sm:text-sm`}
       placeholder={placeholder}
+      onChange={onChange}
     />
   );
 };
