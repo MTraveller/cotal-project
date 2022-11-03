dad41c61803be2639263e5a46deb5e2a236a000d
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-03 04:24:06 +0100

    Refactor: tailwindcss classes for better mobile responsiveness

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,12 +20,12 @@ export default function IndexPage() {
     navigate(`/feed/`)
   ) : (
     <>
-      <div className="w-full flex lg:basis-1/2 items-end lg:items-center px-3 sm:px-0 lg:border-r-2 border-stone-600">
+      <div className="w-full h-[283px] lg:h-auto flex lg:basis-1/2 items-end lg:items-center px-3 sm:px-0 lg:border-r-2 border-stone-600">
         <div className="w-full sm:w-96 lg:w-9/12 2xl:w-1/2 mx-auto">
           <H1Styles className="mb-8 font-black text-slate-300 dark:text-slate-600">
             Welcome to Cotal
           </H1Styles>
-          <p className="text-3xl tracking-wider font-thin text-slate-300 dark:text-slate-600">
+          <p className="mb-8 h-auto lg:mb-0 text-3xl tracking-wider font-thin text-slate-300 dark:text-slate-600">
             Where talents join forces with one another to reach a common goal.
           </p>
         </div>
