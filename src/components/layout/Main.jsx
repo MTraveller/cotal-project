5a0ee4ea7744557407fc05b749c19d4da1dea2c2
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-03 04:20:00 +0100

    Refactor: main styles unhook styled component styles

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,25 +1,26 @@
 import * as React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const IndexMainStyles = styled.main`
-  height: 100%;
+// const IndexMainStyles = styled.main`
+//   height: 100%;
 
-  @media all and (max-width: 1023px) {
-    height: calc(100% - 108px);
-  }
-`;
+//   @media all and (max-width: 1023px) {
+//     height: calc(100% - 108px - 1rem);
+//     /* flex-grow: 2; */
+//   }
+// `;
 
-const MainStyles = styled.main``;
+// const MainStyles = styled.main``;
 
 const Main = ({ children, location }) => {
   return location.pathname === `/` ? (
-    <IndexMainStyles
-      className={`w-full inline-flex flex-wrap content-center justify-center gap-y-14 lg:gap-y-0 lg:bg-stone-900 dark:lg:bg-neutral-900`}
+    <main
+      className={`w-full h-full inline-flex flex-wrap lg:content-center justify-center gap-y-0 lg:bg-stone-900 dark:lg:bg-neutral-900`}
     >
       {children}
-    </IndexMainStyles>
+    </main>
   ) : (
-    <MainStyles>{children}</MainStyles>
+    <main>{children}</main>
   );
 };
 
