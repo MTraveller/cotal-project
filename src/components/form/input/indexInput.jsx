9a48b38909a3cbd1827ae0e2c848a4edaed50dd9
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-04 09:58:45 +0100

    Refactor: all inputs into one shared universal input

--- src/components/form/input/indexInput.jsx
+++ src/components/form/input/indexInput.jsx
@@ -1,72 +1,6 @@
 import React from 'react';
 
-export const TopLoginRegisterInput = ({
-  id,
-  name,
-  type,
-  value,
-  placeholder,
-  onChange,
-}) => {
-  return (
-    <input
-      id={id}
-      name={name}
-      type={type}
-      value={value}
-      required
-      className="relative block w-full appearance-none rounded-none rounded-t-md border border-slate-400 bg-slate-500 px-3 py-3 md:py-4 text-white placeholder-white focus:z-10 focus:border-yellow-300 focus:outline-none focus:ring-yellow-300 sm:text-sm"
-      placeholder={placeholder}
-      onChange={onChange}
-    />
-  );
-};
-
-export const MiddleLoginRegisterInput = ({
-  id,
-  name,
-  type,
-  value,
-  placeholder,
-  onChange,
-}) => {
-  return (
-    <input
-      id={id}
-      name={name}
-      type={type}
-      value={value}
-      required
-      className="relative block w-full appearance-none rounded-none border border-slate-400 bg-slate-500 px-3 py-3 md:py-4 text-white placeholder-white focus:z-10 focus:border-yellow-300 focus:outline-none focus:ring-yellow-300 sm:text-sm"
-      placeholder={placeholder}
-      onChange={onChange}
-    />
-  );
-};
-
-export const BottomLoginRegisterInput = ({
-  id,
-  name,
-  type,
-  value,
-  placeholder,
-  onChange,
-}) => {
-  return (
-    <input
-      id={id}
-      name={name}
-      type={type}
-      value={value}
-      required
-      className="relative block w-full appearance-none rounded-none rounded-b-md border border-slate-400 bg-slate-500 px-3 py-3 md:py-4 text-white placeholder-white focus:z-10 focus:border-yellow-300 focus:outline-none focus:ring-yellow-300 sm:text-sm"
-      placeholder={placeholder}
-      onChange={onChange}
-    />
-  );
-};
-
-export const NameLoginRegisterInput = ({
+export const IndexFormInput = ({
   id,
   name,
   type,
@@ -82,7 +16,7 @@ export const NameLoginRegisterInput = ({
       type={type}
       value={value}
       required
-      className={`relative block w-full appearance-none rounded-none ${borderRadius} border border-slate-400 bg-slate-500 px-3 py-3 md:py-4 text-white placeholder-white focus:z-10 focus:border-yellow-300 focus:outline-none focus:ring-yellow-300 sm:text-sm`}
+      className={`relative block w-full appearance-none ${borderRadius} border border-slate-400 bg-slate-500 px-3 py-3 md:py-4 text-white placeholder-white focus:z-10 focus:border-yellow-300 focus:outline-none focus:ring-yellow-300 sm:text-sm`}
       placeholder={placeholder}
       onChange={onChange}
     />
