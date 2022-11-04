3093fbd45d689d3c3b1ecec12820a4c1bd6d27e5
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-04 10:03:22 +0100

    Add: initial forgot password component

---
+++ src/components/form/Forgot.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+// import Joi from 'joi';
+
+import { handleResetPassword } from '../../services/authService';
+import { IndexFormInput } from './input/indexInput';
+import { SubmitButton } from './indexButton';
+
+const Forgot = ({ form }) => {
+  const [account, setAccount] = useState({ email: '' });
+  const [forgot, setForgot] = useState();
+
+  // const schema = Joi.object({
+  //   email: Joi.string().email({
+  //     minDomainSegments: 2,
+  //     tlds: { allow: false },
+  //   }),
+  //   password: Joi.string()
+  //     .pattern(new RegExp('^[a-zA-Z0-9]{6,30}$'))
+  //     .required(),
+  // });
+
+  // //TODO: validate Forgot form!
+  // const validate = () => {};
+
+  const toggleForm = (e) => form(e.target.name);
+
+  const handleChange = ({ currentTarget: input }) => {
+    setAccount({ ...account, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const forgotRes = await handleResetPassword(account);
+
+    if (forgotRes === `success`) return setForgot(`success`);
+
+    return null;
+  };
+
+  return !forgot === `success` ? (
+    <form className="space-y-6" onSubmit={handleSubmit}>
+      <div className="-space-y-px rounded-md shadow-sm">
+        <div>
+          <label htmlFor="email-address" className="sr-only">
+            Email address
+          </label>
+          <IndexFormInput
+            id="email-address"
+            name="email"
+            type="email"
+            value={account.email}
+            autoComplete="email"
+            borderRadius="rounded-md"
+            placeholder="Email address"
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <button
+            type="button"
+            name="login"
+            className="font-medium text-slate-100 hover:text-slate-300"
+            onClick={toggleForm}
+          >
+            Already have an account?
+          </button>
+        </div>
+
+        <div className="text-sm">
+          <button
+            type="button"
+            name="register"
+            className="font-medium text-slate-100 hover:text-slate-300"
+            onClick={toggleForm}
+          >
+            New to Cotal?
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <SubmitButton buttonText="Submit" />
+      </div>
+    </form>
+  ) : (
+    <div className="flex flex-col items-center gap-5">
+      <h2 className="text-center text-slate-500 text-xl">
+        Success, check your inbox for instruction
+      </h2>
+      <button
+        type="button"
+        name="login"
+        className="w-52 font-medium bg-teal-500 hover:bg-slate-600 p-2 rounded-md text-slate-900 hover:text-teal-500"
+        onClick={toggleForm}
+      >
+        Back!
+      </button>
+    </div>
+  );
+};
+
+export default Forgot;
