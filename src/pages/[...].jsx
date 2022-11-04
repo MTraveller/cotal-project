a7dda33c7e522f1baa8b3f8ed24fc6e379b26a98
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-04 03:56:42 +0100

    Add: private routes user network and user setting

--- src/pages/[...].jsx
+++ src/pages/[...].jsx
@@ -3,7 +3,9 @@ import { Router } from '@reach/router';
 
 import PrivateRoute from '../components/PrivateRoute';
 import Feed from '../components/page/Feed';
+import UserNetwork from '../components/page/UserNetwork';
 import ProfileDetail from '../components/page/ProfileDetail';
+import UserSetting from '../components/page/UserSetting';
 import IndexPage from './index';
 import NotFoundPage from './404';
 
@@ -13,7 +15,9 @@ function App({ params }) {
   return (
     <Router>
       <PrivateRoute path="/feed/" component={Feed} />
+      <PrivateRoute path="/my-network/" component={UserNetwork} />
       <PrivateRoute path="/profile/" component={ProfileDetail} />
+      <PrivateRoute path="/settings/" component={UserSetting} />
       <IndexPage path="/" />
       <NotFoundPage path={`/${currentPath}`} />
     </Router>
