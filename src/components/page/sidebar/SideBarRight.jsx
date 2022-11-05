79ab959ce0d6cc5680f840db08f42f7f3ad839fe
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-05 17:40:20 +0100

    Add: initial right side bar component

---
+++ src/components/page/sidebar/SideBarRight.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideBarRight = () => {
+  return (
+    <div className="hidden xl:block flex-none w-80">
+      <p>Tags from SidebarRight</p>
+    </div>
+  );
+};
+
+export default SideBarRight;
