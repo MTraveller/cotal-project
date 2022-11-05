d647dbb6086761e2b438846454a89c4e74331a19
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-05 17:47:16 +0100

    Add: styles to main html tag not route / & remove commented out code

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,26 +1,20 @@
 import * as React from 'react';
-// import styled from 'styled-components';
+import tw from 'tailwind-styled-components';
 
-// const IndexMainStyles = styled.main`
-//   height: 100%;
-
-//   @media all and (max-width: 1023px) {
-//     height: calc(100% - 108px - 1rem);
-//     /* flex-grow: 2; */
-//   }
-// `;
-
-// const MainStyles = styled.main``;
+const MainStyles = tw.main`
+  dark:border-t
+  border-slate-700
+  dark:text-slate-400
+  pt-7
+`;
 
 const Main = ({ children, location }) => {
   return location.pathname === `/` ? (
-    <main
-      className={`w-full h-full inline-flex flex-wrap lg:content-center justify-center gap-y-0 lg:bg-stone-900 dark:lg:bg-neutral-900`}
-    >
+    <main className="w-full h-full inline-flex flex-wrap lg:content-center justify-center gap-y-0 lg:bg-stone-900 dark:lg:bg-neutral-900">
       {children}
     </main>
   ) : (
-    <main>{children}</main>
+    <MainStyles>{children}</MainStyles>
   );
 };
 
