270a1a20777659ec3569365964f5719264478031
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-05 17:40:34 +0100

    Add: initial left side bar component

---
+++ src/components/page/sidebar/SideBarLeft.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideBarLeft = () => {
+  return (
+    <div className="hidden xl:block flex-none w-80">
+      <p>Tags from Sidebarleft</p>
+    </div>
+  );
+};
+
+export default SideBarLeft;
