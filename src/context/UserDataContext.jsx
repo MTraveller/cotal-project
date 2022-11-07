77db25697946dc2e2b343cbcfa621012effbcc8e
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-07 14:58:43 +0100

    Add: initial user data context component

---
+++ src/context/UserDataContext.jsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+import { getUser } from '../services/authService';
+import getUserDatahandler from '../api/users/userData';
+
+export const UserDataContext = createContext({
+  userData: {},
+});
+
+export const UserDataProvider = ({ children }) => {
+  const [userData, setUserData] = useState();
+
+  useEffect(() => {
+    getUserDatahandler(getUser().access)
+      .then((res) => {
+        return setUserData(res.data);
+      })
+      .catch((ex) => {
+        return ex;
+      });
+  }, []);
+
+  return (
+    <UserDataContext.Provider value={{ userData }}>
+      {children}
+    </UserDataContext.Provider>
+  );
+};
+
+export const useUserDataContext = () => useContext(UserDataContext);
