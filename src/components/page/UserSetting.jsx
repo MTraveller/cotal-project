895870d1fa0b95cb9a58ad608e449723806dc3f6
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-07 18:03:33 +0100

    Cleanup: remove commented out code & return statement

--- src/components/page/UserSetting.jsx
+++ src/components/page/UserSetting.jsx
@@ -1,23 +1,17 @@
-import * as React from 'react';
-// import { Link } from 'gatsby';
+import React from 'react';
 
-// import http from '../services/httpService';
 import Seo from '../Seo';
 
-const UserSetting = (rest) => {
-  // console.log('UserSettings');
-  // console.log(rest);
-  return (
-    <>
-      <h1>
-        This page is <b>User Settings Page</b>
-      </h1>
-      <p>
-        This page is for the user <code>hello</code> on the DRF db.
-      </p>
-    </>
-  );
-};
+const UserSetting = ({ userData }) => (
+  <>
+    <h1>
+      This page is <b>User Settings Page</b>
+    </h1>
+    <p>
+      This page is for the user <code>hello</code> on the DRF db.
+    </p>
+  </>
+);
 
 export const Head = () => <Seo title="User Settings" />;
 
