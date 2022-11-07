62d40dabfe7966cad6240528342ffa7c8de50a26
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-07 14:56:06 +0100

    Add: bg to div

--- src/components/page/sidebar/SideBarLeft.jsx
+++ src/components/page/sidebar/SideBarLeft.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SideBarLeft = () => {
   return (
-    <div className="hidden xl:block flex-none w-80">
+    <div className="hidden xl:block flex-none w-80 bg-black/[.2] rounded-md">
       <p>Tags from Sidebarleft</p>
     </div>
   );
--- src/components/page/sidebar/SideBarRight.jsx
+++ src/components/page/sidebar/SideBarRight.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SideBarRight = () => {
   return (
-    <div className="hidden xl:block flex-none w-80">
+    <div className="hidden xl:block flex-none w-80 bg-black/[.2] rounded-md">
       <p>Tags from SidebarRight</p>
     </div>
   );
