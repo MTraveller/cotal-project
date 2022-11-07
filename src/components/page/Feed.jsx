0d4942ed40229dfde90ef6cc4bedc62755d85257
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-07 14:55:15 +0100

    Add: styles to div & add userData context

--- src/components/page/Feed.jsx
+++ src/components/page/Feed.jsx
@@ -1,23 +1,29 @@
-import * as React from 'react';
+import React from 'react';
 // import { Link } from 'gatsby';
 import tw from 'tailwind-styled-components';
 
-import Seo from '../Seo';
 import SideBarLeft from './sidebar/SideBarLeft';
 import SideBarRight from './sidebar/SideBarRight';
+import Seo from '../Seo';
+import { useUserDataContext } from '../../context/UserDataContext';
 
 const PageWrapper = tw.div`
   flex
   flex-row
   flex-wrap
   justify-center
+  gap-6
 `;
 
 const Feed = () => {
+  const { userData } = useUserDataContext();
+
+  console.log(userData);
+
   return (
     <PageWrapper>
       <SideBarLeft />
-      <div className="flex-auto w-[500px] max-w-3xl">
+      <div className="flex-auto w-[500px] max-w-3xl bg-black/[.2] rounded-md">
         <h1>Feed Page</h1>
         <p>
           This feed page is for the user <code>hello</code> on the DRF db.
