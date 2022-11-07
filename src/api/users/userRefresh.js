ce0857400660c98981974c6d3a756d202c389ab1
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-07 14:45:33 +0100

    Add: initial user api access token refresh

---
+++ src/api/users/userRefresh.js
@@ -0,0 +1,22 @@
+import http from '../../services/httpService';
+
+const apiRefreshEndpoint = `${process.env.GATSBY_API_URL}/auth/jwt/refresh`;
+
+export default async function userLoginHandler(req, res) {
+  const result = await http
+    .post(apiRefreshEndpoint, {
+      refresh: req,
+    })
+    .then((res) => {
+      return res;
+    })
+    .catch((ex) => {
+      return ex;
+    });
+
+  const status = result.response?.status;
+
+  if (status === 401) return status;
+
+  return result;
+}
