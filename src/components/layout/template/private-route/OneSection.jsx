e88bd205f9677f4ec15502772fb56872822993b1
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-09 06:07:51 +0100

    Add: initial one section component

---
+++ src/components/layout/template/private-route/OneSection.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import tw from 'tailwind-styled-components';
+
+const Wrapper = tw.div`
+  max-w-7xl
+  mx-auto
+  px-4
+  sm:px-6
+  lg:px-8
+`;
+
+const DivStyles = tw.div`
+  w-full
+  flex
+  flex-flow
+  flex-wrap
+  gap-10
+  lg:gap-20
+  xl:gap-40
+  mt-8
+`;
+
+export const OneSection = ({ userData, title, component: Component }) => (
+  <Wrapper>
+    <h1 className="text-3xl">{title}</h1>
+    <DivStyles>
+      <Component userData={userData} />
+    </DivStyles>
+  </Wrapper>
+);
