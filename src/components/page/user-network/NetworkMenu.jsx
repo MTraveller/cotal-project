187ef26554fc50491245df02e3948c817d95785a
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-09 06:16:16 +0100

    Add: initial network menu component

--- src/components/page/user-network/NetworkMenu.jsx
+++ src/components/page/user-network/NetworkMenu.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import tw from 'tailwind-styled-components';
+// import tw from 'tailwind-styled-components';
 
 export const NetworkMenu = () => (
-  <>
+  <div className="bg-black/[.2] rounded-lg p-6">
     <p>Hello from Network Menu</p>
-  </>
+  </div>
 );
