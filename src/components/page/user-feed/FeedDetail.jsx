88da43c37e7ec55c9558b6467979c2b09a2644fe
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-09 06:15:33 +0100

    Add: initial feed detail component

---
+++ src/components/page/user-feed/FeedDetail.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import tw from 'tailwind-styled-components';
+
+export const FeedDetail = () => (
+  <div className="bg-black/[.2] rounded-lg p-6">
+    <p>
+      This feed page is for the user <code>hello</code> on the DRF db.
+    </p>
+  </div>
+);
