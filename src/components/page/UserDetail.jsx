484849bb24fb0ad26f120bfa425369095d7938b2
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-09 06:09:49 +0100

    Refactor: update two section template import & optimize the styles for each section

--- src/components/page/UserDetail.jsx
+++ src/components/page/UserDetail.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { TwoSection } from '../layout/template/TwoSection';
+import { TwoSection } from '../layout/template/private-route/TwoSection';
 import { PersonalDetail } from './user-detail/PersonalDetail';
 import { PublicDetail } from './user-detail/PublicDetail';
 import Seo from '../Seo';
@@ -9,8 +9,8 @@ const UserDetail = ({ userData }) => (
   <TwoSection
     title="Your Profile"
     userData={userData}
-    leftStyles="lg:basis-60"
-    rightStyles="lg:basis-auto"
+    leftStyles="w-full sm:flex-initial sm:w-80 sm:max-w-80"
+    rightStyles="flex-auto w-[590px] lg:max-w-[1200px]"
     componentLeft={PersonalDetail}
     componentRight={PublicDetail}
   />
