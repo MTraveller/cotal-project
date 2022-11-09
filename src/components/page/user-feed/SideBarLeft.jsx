1ffc2a67e0295a22637c192e073032587f8c0d58
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-09 06:13:58 +0100

    Move: component to user feed folder

---
+++ src/components/page/user-feed/SideBarLeft.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SideBarLeft = () => {
+  return (
+    <div className="bg-black/[.2] rounded-lg p-6">
+      <p>Tags from Sidebarleft</p>
+    </div>
+  );
+};
---
+++ src/components/page/user-feed/SideBarRight.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SideBarRight = () => {
+  return (
+    <div className="bg-black/[.2] rounded-lg p-6">
+      <p>Tags from SidebarRight</p>
+    </div>
+  );
+};
