e1968d91be85facd682ce9888349093cf91ee4dd
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-09 06:15:58 +0100

    Add: initial network detail component

--- src/components/page/user-network/NetworkDetail.jsx
+++ src/components/page/user-network/NetworkDetail.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import tw from 'tailwind-styled-components';
+// import tw from 'tailwind-styled-components';
 
 export const NetworkDetail = () => (
-  <>
+  <div className="bg-black/[.2] rounded-lg p-6">
     <p>Hello from Network detail</p>
-  </>
+  </div>
 );
