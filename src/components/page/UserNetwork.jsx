fb59b827ba5fe8eced101b736de535f0456f4495
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-09 06:11:31 +0100

    Refactor: update two section template import & optimize the styles for each section

--- src/components/page/UserNetwork.jsx
+++ src/components/page/UserNetwork.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { TwoSection } from '../layout/template/TwoSection';
+import { TwoSection } from '../layout/template/private-route/TwoSection';
 import { NetworkMenu } from './user-network/NetworkMenu';
 import { NetworkDetail } from './user-network/NetworkDetail';
 import Seo from '../Seo';
@@ -9,8 +9,8 @@ const UserNetwork = ({ userData }) => (
   <TwoSection
     title="My Network"
     userData={userData}
-    leftStyles="lg:basis-80"
-    rightStyles="lg:basis-auto"
+    leftStyles="flex-1 lg:flex-none lg:max-w-60"
+    rightStyles="flex-auto w-[451px] lg:max-w-[1200px]"
     componentLeft={NetworkMenu}
     componentRight={NetworkDetail}
   />
