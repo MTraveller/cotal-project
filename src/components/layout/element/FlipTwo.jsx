5f045a3cd267ca4f0773a37078833534538c0932
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-10 22:03:08 +0100

    Add: initial flip two component

---
+++ src/components/layout/element/FlipTwo.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { Input } from '../../form/input/Input';
+import { handleEditSaveFlip } from '../../../utils/handleEditSaveFlip';
+
+export const FlipTwo = ({
+  paragraphHight,
+  text,
+  link,
+  spanId,
+  variable,
+  inputId,
+  inputName,
+  inputType,
+  inputValue,
+  inputAutocomplete,
+  inputBorderRadius,
+  inputPadding,
+  inputPlaceholder,
+  onChange,
+}) => (
+  <div className="flex justify-between items-center">
+    <p className={`flex ${paragraphHight} items-center gap-3`}>
+      {text}:{` `}
+      {link ? (
+        <a id={spanId} href={link} target="_blank" rel="noopener noreferrer">
+          {variable}
+        </a>
+      ) : (
+        <span id={spanId}>{variable}</span>
+      )}
+    </p>
+    <span id={inputId} name={inputName} className="hidden w-3/4">
+      <Input
+        id={inputId}
+        name={inputName}
+        type={inputType}
+        value={inputValue}
+        autoComplete={inputAutocomplete}
+        borderRadius={inputBorderRadius}
+        padding={inputPadding}
+        placeholder={inputPlaceholder}
+        onChange={onChange}
+      />
+    </span>
+    <button type="button" onClick={handleEditSaveFlip}>
+      Edit
+    </button>
+  </div>
+);
