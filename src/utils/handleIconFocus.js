29c4edb325664f9c47a908596ee33dc2cdf7f555
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-10 22:05:42 +0100

    Add: initial handle icon focus component

---
+++ src/utils/handleIconFocus.js
@@ -0,0 +1,22 @@
+export const handleIconFocus = (e) => {
+  const clickedButton =
+    e.target.type === `button`
+      ? e.target
+      : e.target.parentNode.type === `button`
+      ? e.target.parentNode
+      : e.target.parentNode.parentNode;
+
+  const parentDivChildren = clickedButton.parentNode.parentNode.childNodes;
+
+  Array.from(parentDivChildren).forEach((item) => {
+    if (item.id !== clickedButton.id) {
+      item.classList.replace(`flex`, `hidden`);
+      if (item.id === `icon-save-button`) {
+        item.classList.replace(`hidden`, `flex`);
+      }
+    } else {
+      item.classList.add(`w-3/4`, `gap-3`);
+      item.lastChild.classList.replace(`hidden`, `flex`);
+    }
+  });
+};
