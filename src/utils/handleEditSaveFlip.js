9066d7a60657212de5d7309c2bfdb5561b821573
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-10 22:04:22 +0100

    Add: animation to swapping of elements

--- src/utils/handleEditSaveFlip.js
+++ src/utils/handleEditSaveFlip.js
@@ -3,14 +3,38 @@ export const handleEditSaveFlip = (e) => {
   innerText = innerText === `Edit` ? `Save` : `Edit`;
   e.target.innerText = innerText;
 
-  let firstChild = e.target.parentElement.firstChild;
-  let firstChildClassList = firstChild.classList;
-  let siblingClassList = firstChild.nextElementSibling.classList;
+  const firstChild = e.target.parentElement.firstChild;
+  const sibling = firstChild.nextElementSibling;
+  const firstChildClassList = firstChild.classList;
+  const siblingClassList = firstChild.nextElementSibling.classList;
 
-  const isBlock = firstChildClassList.contains('flex');
-  const block = isBlock ? 'flex' : 'hidden';
-  const hidden = isBlock ? 'hidden' : 'flex';
+  const isFlex = firstChildClassList.contains(`flex`);
+  const block = isFlex ? `flex` : `hidden`;
+  const hidden = isFlex ? `hidden` : `flex`;
 
-  firstChildClassList.replace(block, hidden);
-  siblingClassList.replace(hidden, block);
+  // Amimation learning material from:
+  // https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API
+  const swapKeyframes = new KeyframeEffect(
+    isFlex ? firstChild : sibling,
+    [{ opacity: 0 }],
+    {
+      duration: 100,
+      easing: `ease-in-out`,
+    }
+  );
+
+  const swapAnimation = new Animation(swapKeyframes, document.timeline);
+
+  swapAnimation.play();
+  swapAnimation.finished
+    .then(() => {
+      isFlex
+        ? firstChildClassList.replace(block, hidden)
+        : siblingClassList.replace(hidden, block);
+    })
+    .then(() => {
+      isFlex
+        ? siblingClassList.replace(hidden, block)
+        : firstChildClassList.replace(block, hidden);
+    });
 };
