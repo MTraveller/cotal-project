393f268cabd8ece1cfde47566000534a10117eb3
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-12 23:22:22 +0100

    Add: code to handle button clicks for new browser tab

--- src/utils/handleKeypress.js
+++ src/utils/handleKeypress.js
@@ -1,7 +1,13 @@
 export const handleKeypress = (e) => {
   const htmlFor = e.target.firstChild.htmlFor;
 
-  if (e.keyCode === 32 || e.keyCode === 13) {
-    return document.getElementById(htmlFor).click();
+  const link = htmlFor ? `` : e.target.firstChild.href;
+
+  if (e.code === `Space` || e.code === `Enter` || e.type === `click`) {
+    if (htmlFor) {
+      return document.activeElement.firstChild.click();
+    } else if (e.target.type === `button`) {
+      return window.open(link, `_blank`);
+    }
   }
 };
