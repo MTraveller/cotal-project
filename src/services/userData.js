d3bdbbbd662e8baa8e8bfd8f83f585cffdac9bea
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-12 23:20:29 +0100

    Remove: res arg after move, not needed

--- src/services/userData.js
+++ src/services/userData.js
@@ -3,7 +3,7 @@ import { handleTokenRefresh } from './authService';
 
 const apiProfileMeEndpoint = `${process.env.GATSBY_API_URL}/profiles/me/`;
 
-export default async function getUserDataHandler(req, res) {
+export default async function getUserDataHandler(req) {
   // GET user data from api
   const result = await http
     .get(apiProfileMeEndpoint, {
