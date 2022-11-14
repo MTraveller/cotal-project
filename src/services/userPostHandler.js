5c758e2cdf09098875f79ead53ac9f94bfa84ece
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-14 20:24:18 +0100

    Add: initial userPostHandler file

---
+++ src/services/userPostHandler.js
@@ -0,0 +1,19 @@
+import http from './httpService';
+
+export async function userProfileHandler({ data }) {
+  console.log(data);
+
+  // const res = await http.post()
+}
+
+export async function userSocialHandler({ data }) {
+  console.log(data);
+
+  // const res = await http.post()
+}
+
+export async function userLinktreesHandler({ data }) {
+  console.log(data);
+
+  // const res = await http.post()
+}
