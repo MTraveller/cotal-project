f7f9c4f900a87648bfd36faef181dd9ce5da3065
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-14 20:23:36 +0100

    Add: initial postDataHandler()

--- src/services/postData.js
+++ src/services/postData.js
@@ -1,5 +1,20 @@
-import http from './httpService';
+import {
+  userProfileHandler,
+  userSocialHandler,
+  userLinktreesHandler,
+} from './userPostHandler';
 
 export default async function postDataHandler(req) {
-  console.log(req);
+  if (req[0] === `personal`) {
+    const profileLength = Object.keys(req[2]).length;
+
+    if (req[2].socials) await userSocialHandler({ data: req[2].socials });
+    if (req[2].linktrees)
+      await userLinktreesHandler({ data: req[2].linktrees });
+
+    delete req[2].socials;
+    delete req[2].linktrees;
+
+    if (profileLength > 0) await userProfileHandler({ data: req[2] });
+  }
 }
