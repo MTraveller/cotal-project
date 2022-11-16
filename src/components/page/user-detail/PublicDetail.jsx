ee8cec70d8bb806baad8e3a47e7ee8db9f2d5aca
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-16 03:17:55 +0100

    Add: profile portfolio, certificate, award and creative components

--- src/components/page/user-detail/PublicDetail.jsx
+++ src/components/page/user-detail/PublicDetail.jsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AddNew } from './public-detail/AddNew';
+import { Model } from './public-detail/Model';
 
-export const PublicDetail = () => (
-  <div className="bg-black/[.2] rounded-lg p-6">
-    <p>Hello from PublicDetails</p>
-  </div>
-);
+export const PublicDetail = () => {
+  const [active, setActive] = useState(false);
+  const [targetName, setTargetName] = useState();
+
+  const handleClick = ({ target }) => {
+    const parentDiv = target.parentNode.parentNode.parentNode;
+
+    parentDiv.classList.add(`hidden`);
+
+    const model = target.previousSibling.innerText.slice(5, -1);
+
+    setTargetName(model);
+    setActive(true);
+  };
+  return (
+    <div className="bg-black/[.2] rounded-lg p-6">
+      {active ? (
+        <AddNew setActive={setActive} model={targetName} />
+      ) : (
+        <div className="flex flex-col gap-y-10">
+          <div>
+            <div className="flex flex-row mb-3 justify-between items-center text-sm">
+              <h2>Your Portfolios</h2>
+              <button type="button" onClick={handleClick}>
+                Add new
+              </button>
+            </div>
+            <Model model="portfolios" />
+          </div>
+          <div>
+            <div className="flex flex-row mb-3 justify-between items-center text-sm">
+              <h2>Your Awards</h2>
+              <button type="button" onClick={handleClick}>
+                Add new
+              </button>
+            </div>
+            <Model model="awards" />
+          </div>
+          <div>
+            <div className="flex flex-row mb-3 justify-between items-center text-sm">
+              <h2>Your Certificates</h2>
+              <button type="button" onClick={handleClick}>
+                Add new
+              </button>
+            </div>
+            <Model model="certificates" />
+          </div>
+          <div>
+            <div className="flex flex-row mb-3 justify-between items-center text-sm">
+              <h2>Your Creatives</h2>
+              <button type="button" onClick={handleClick}>
+                Add new
+              </button>
+            </div>
+            <Model model="creatives" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
