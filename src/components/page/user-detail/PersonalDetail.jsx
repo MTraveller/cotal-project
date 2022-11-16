d1db41985345d43b4d836463200fb0f76a2a85d9
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-16 22:56:50 +0100

    Add: button dropdown with transition for profile details on click

--- src/components/page/user-detail/PersonalDetail.jsx
+++ src/components/page/user-detail/PersonalDetail.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { MdMenuOpen } from 'react-icons/md';
 
 import { useUserDataContext } from '../../../context/UserDataContext';
 import { checkEquality } from '../../../utils/checkEquality';
@@ -51,6 +52,17 @@ export const PersonalDetail = () => {
     }
   }, [userData, personal.image, personal.status]);
 
+  const handleDivClick = ({ currentTarget: current }) => {
+    current.parentNode.classList.toggle(`h-[90px]`);
+
+    current.nextSibling.classList.toggle(`invisible`);
+
+    current.dataset.open = current.dataset.open === `false` ? `true` : `false`;
+
+    current.nextSibling.dataset.hidden =
+      current.nextSibling.dataset.hidden === `true` ? `false` : `true`;
+  };
+
   const onSelectFile = (e) => {
     if (!e.target.files || e.target.files.length === 0) return;
 
@@ -101,86 +113,99 @@ export const PersonalDetail = () => {
   };
 
   return (
-    <div className="lg:sticky lg:top-[78px] bg-black/[.2] rounded-lg p-6">
-      <form className="flex flex-col gap-y-2">
-        <div className="flex flex-row justify-between items-center">
-          <div className="w-32">
-            <ButtonImageInput
-              id="label-profile-image"
-              preview={preview ? preview : personal?.image}
-              alt={`${personal?.user?.first_name} ${personal?.user?.last_name}`}
-              svg={<ProfileImageSvg widthHeight="w-32 h-32" />}
-              onChange={onSelectFile}
-            />
+    <div className="h-[90px] lg:sticky lg:top-[78px] bg-black/[.2] rounded-lg p-6 overflow-y-hidden">
+      <button
+        type="button"
+        className="group w-full flex justify-center items-center gap-x-2 py-1 dark:bg-slate-900 rounded-md ring-1 dark:ring-slate-400 transition ease-in-out delay-150 hover:ring-1 hover:dark:ring-yellow-400 hover:ring-offset-4 hover:ring-offset-gray-900"
+        data-open="false"
+        onClick={handleDivClick}
+      >
+        <span className="group-data-[open=true]:-rotate-90 ease-in-out duration-500">
+          <MdMenuOpen size={24} />
+        </span>{' '}
+        Profile Details
+      </button>
+      <div className="group mt-5 invisible" data-hidden="true">
+        <form className="flex flex-col gap-y-2 transform translate-y-20 opacity-0 group-data-[hidden=false]:translate-y-0 group-data-[hidden=false]:opacity-100 transition ease-in-out duration-700">
+          <div className="flex flex-row justify-between items-center">
+            <div className="w-32">
+              <ButtonImageInput
+                id="label-profile-image"
+                preview={preview ? preview : personal?.image}
+                alt={`${personal?.user?.first_name} ${personal?.user?.last_name}`}
+                svg={<ProfileImageSvg widthHeight="w-32 h-32" />}
+                onChange={onSelectFile}
+              />
+            </div>
+            <div className="min-w-max flex flex-col place-items-end gap-5">
+              <OpenLinkExternal
+                url={`/in/${userData?.slug}/`}
+                buttonText="View Profile"
+              />
+              <ButtonLabelInput
+                id="change-profile-image-button"
+                svg={<UploadSvg widthHeight="w-6 h-6" />}
+                text="Add Image"
+                onChange={onSelectFile}
+              />
+            </div>
           </div>
-          <div className="min-w-max flex flex-col place-items-end gap-5">
-            <OpenLinkExternal
-              url={`/in/${userData?.slug}/`}
-              buttonText="View Profile"
-            />
-            <ButtonLabelInput
-              id="change-profile-image-button"
-              svg={<UploadSvg widthHeight="w-6 h-6" />}
-              text="Add Image"
-              onChange={onSelectFile}
-            />
-          </div>
-        </div>
-        <FlipSelectTwo
-          divHeight="h-[47px]"
-          inputText="Status"
-          spanId="status"
-          inputValue={personal.status}
-          onChange={handleStatusSelect}
-          dataMsg="Edit your status"
-        />
-        <FlipTwo
-          divHeight="h-[47px]"
-          text="Location"
-          spanId="location"
-          inputId="location"
-          inputName="location"
-          inputType="text"
-          inputValue={personal.location ? personal.location : ``}
-          inputAutocomplete="country-name"
-          inputDisplay="block"
-          inputBorderRadius="rounded-md"
-          inputPadding="py-2 pl-6 pr-10"
-          inputPlaceholder="Location"
-          onChange={handleChange}
-          dataMsg="Edit your location"
-        />
-        <FlipTwo
-          divHeight="h-[47px]"
-          text="LinkTree"
-          link={`https://linktr.ee/${personal.linktrees}`}
-          spanId="linktrees"
-          inputId="linktrees"
-          inputName="linktrees"
-          inputType="text"
-          inputValue={personal.linktrees ? personal.linktrees : ``}
-          inputAutocomplete="off"
-          inputDisplay="block"
-          inputBorderRadius="rounded-md"
-          inputPadding="py-2 pl-6 pr-10"
-          inputPlaceholder="Username"
-          onChange={handleChange}
-          dataMsg="Edit your linktree username"
-        />
-        <SocialInput
-          divHeight="h-[47px]"
-          inputId="socials"
-          inputType="text"
-          inputValue={personal.socials}
-          inputAutocomplete="off"
-          inputDisplay="hidden"
-          inputBorderRadius="rounded-md"
-          inputPadding="py-2 pl-6 pr-10"
-          inputPlaceholder="Username"
-          onChange={handleChange}
-        />
-        <FormButton handleSubmit={handleSubmit} buttonText="Save Changes" />
-      </form>
+          <FlipSelectTwo
+            divHeight="h-[47px]"
+            inputText="Status"
+            spanId="status"
+            inputValue={personal.status}
+            onChange={handleStatusSelect}
+            dataMsg="Edit your status"
+          />
+          <FlipTwo
+            divHeight="h-[47px]"
+            text="Location"
+            spanId="location"
+            inputId="location"
+            inputName="location"
+            inputType="text"
+            inputValue={personal.location ? personal.location : ``}
+            inputAutocomplete="country-name"
+            inputDisplay="block"
+            inputBorderRadius="rounded-md"
+            inputPadding="py-2 pl-6 pr-10"
+            inputPlaceholder="Location"
+            onChange={handleChange}
+            dataMsg="Edit your location"
+          />
+          <FlipTwo
+            divHeight="h-[47px]"
+            text="LinkTree"
+            link={`https://linktr.ee/${personal.linktrees}`}
+            spanId="linktrees"
+            inputId="linktrees"
+            inputName="linktrees"
+            inputType="text"
+            inputValue={personal.linktrees ? personal.linktrees : ``}
+            inputAutocomplete="off"
+            inputDisplay="block"
+            inputBorderRadius="rounded-md"
+            inputPadding="py-2 pl-6 pr-10"
+            inputPlaceholder="Username"
+            onChange={handleChange}
+            dataMsg="Edit your linktree username"
+          />
+          <SocialInput
+            divHeight="h-[47px]"
+            inputId="socials"
+            inputType="text"
+            inputValue={personal.socials}
+            inputAutocomplete="off"
+            inputDisplay="hidden"
+            inputBorderRadius="rounded-md"
+            inputPadding="py-2 pl-6 pr-10"
+            inputPlaceholder="Username"
+            onChange={handleChange}
+          />
+          <FormButton handleSubmit={handleSubmit} buttonText="Save Changes" />
+        </form>
+      </div>
     </div>
   );
 };
