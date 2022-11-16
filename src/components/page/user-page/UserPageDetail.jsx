6083facc0208e3cb0316f6c0378be847639ba598
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-16 12:55:55 +0100

    Add: initial UserPageDetail() component

---
+++ src/components/page/user-page/UserPageDetail.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import tw from 'tailwind-styled-components';
+
+export const UserPageDetail = () => (
+  <div className="bg-black/[.2] rounded-lg p-6">
+    <p>Hello from user page detail</p>
+  </div>
+);
