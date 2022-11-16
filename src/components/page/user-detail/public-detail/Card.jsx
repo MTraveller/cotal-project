96db0900b9244fe8f90c0974f2c6a38f9202b8c1
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-16 11:08:33 +0100

    Refactor: add slugify code of post title to open in new tab & make card as a button and compatible with W3C html markup

--- src/components/page/user-detail/public-detail/Card.jsx
+++ src/components/page/user-detail/public-detail/Card.jsx
@@ -3,24 +3,43 @@ import React from 'react';
 import { Image } from '../../../layout/element/Image';
 
 export const Card = ({ model, modelName, userSlug }) => {
+  // https://github.com/30-seconds/30-seconds-of-code/blob/master/snippets/slugify.md
+  const slugify = (title) =>
+    title
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/[\s_-]+/g, '-')
+      .replace(/^-+|-+$/g, '');
+
+  const handleClick = ({ currentTarget }) => {
+    const title = currentTarget.name;
+    window.open(`/in/${userSlug}/${slugify(title)}/`, `_blank`);
+  };
+
   return model?.length > 0 ? (
     <div className="overflow-x-auto flex flex-row gap-x-10">
       {model?.map((item) => (
-        <div key={item.id} className="bg-slate-900 rounded-md mb-4">
-          <div className="flex flex-col w-80">
-            <h2 className="py-2 px-4">{item.title}</h2>
-            <figure>
-              <Image
-                image={item.image}
-                modelName={modelName}
-                userSlug={userSlug}
-              />
-              <figcaption className="relative flex items-center -mt-8 px-4 py-2 rounded-b-md bg-slate-900 text-sm">
-                {item.link}
-              </figcaption>
-            </figure>
-          </div>
-        </div>
+        <button
+          key={item.id}
+          type="button"
+          name={item.title}
+          className="bg-slate-900 rounded-md mb-4"
+          onClick={handleClick}
+        >
+          <span className="flex flex-col w-80 text-left">
+            <span className="py-2 px-4">{item.title}</span>
+            <Image
+              image={item.image}
+              modelName={modelName}
+              userSlug={userSlug}
+              alt={item.title}
+            />
+            <span className="relative flex items-center -mt-8 px-4 py-2 rounded-b-md bg-slate-900 text-sm">
+              {item.link}
+            </span>
+          </span>
+        </button>
       ))}
     </div>
   ) : (
