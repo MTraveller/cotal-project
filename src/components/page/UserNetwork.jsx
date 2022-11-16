f70cbaff139b3ea31199b3a168657218433c1c2a
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-16 12:52:32 +0100

    Refactor: update two section component after seperation

--- src/components/page/UserNetwork.jsx
+++ src/components/page/UserNetwork.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import { TwoSection } from '../layout/template/private-route/TwoSection';
+import { TwoSectionLeft } from '../layout/template/private-route/TwoSectionLeft';
 import { NetworkMenu } from './user-network/NetworkMenu';
 import { NetworkDetail } from './user-network/NetworkDetail';
 import Seo from '../Seo';
 
-const UserNetwork = ({ userData }) => (
-  <TwoSection
+const UserNetwork = () => (
+  <TwoSectionLeft
     title="My Network"
-    userData={userData}
     leftStyles="flex-1 lg:flex-none lg:max-w-60"
     rightStyles="flex-auto w-[451px] lg:max-w-[1200px]"
     componentLeft={NetworkMenu}
