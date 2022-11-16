cf4f68e37281266297f9b221ba3afd13c5f98b90
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-16 03:06:48 +0100

    Add: initial textarea component

---
+++ src/components/form/input/Textarea.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const Textarea = ({
+  id,
+  name,
+  type,
+  dataValue,
+  value,
+  autoComplete,
+  display,
+  borderRadius,
+  padding,
+  placeholder,
+  onChange,
+}) => {
+  return (
+    <textarea
+      id={id}
+      name={name}
+      type={type}
+      data-name={dataValue}
+      value={value ? value : ``}
+      autoComplete={autoComplete}
+      required
+      className={`relative ${display} w-full appearance-none ${borderRadius} border border-slate-400 bg-slate-500 ${padding} text-white placeholder-white focus:z-10 focus:border-yellow-300 focus:outline-none focus:ring-yellow-300 sm:text-sm`}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  );
+};
