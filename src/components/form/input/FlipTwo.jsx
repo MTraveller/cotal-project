cc4280e5066498f3a8c7519b32a83655474e1bab
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-16 03:06:22 +0100

    Move: component under input folder

---
+++ src/components/form/input/FlipTwo.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { Input } from '../../form/input/Input';
+import { InputEditButton } from '../../form/InputEditButton';
+import { handleEditSaveFlip } from '../../../utils/handleEditSaveFlip';
+
+export const FlipTwo = ({
+  divHeight,
+  paragraphHight,
+  text,
+  link,
+  spanId,
+  inputId,
+  inputName,
+  inputType,
+  inputValue,
+  inputAutocomplete,
+  inputBorderRadius,
+  inputPadding,
+  inputPlaceholder,
+  onChange,
+  dataMsg,
+}) => (
+  <div className={`flex justify-between items-center ${divHeight}`}>
+    <p className={`flex ${paragraphHight} items-center gap-3`}>
+      {text}:{` `}
+      {link ? (
+        <a id={spanId} href={link} target="_blank" rel="noopener noreferrer">
+          {inputValue}
+        </a>
+      ) : (
+        <span id={spanId}>{inputValue}</span>
+      )}
+    </p>
+    <span id={inputId} name={inputName} className="hidden w-3/4">
+      <Input
+        id={inputId}
+        name={inputName}
+        type={inputType}
+        value={inputValue}
+        autoComplete={inputAutocomplete}
+        borderRadius={inputBorderRadius}
+        padding={inputPadding}
+        placeholder={inputPlaceholder}
+        onChange={onChange}
+      />
+    </span>
+    <InputEditButton
+      handleEditSaveFlip={handleEditSaveFlip}
+      dataMsg={dataMsg}
+    />
+  </div>
+);
