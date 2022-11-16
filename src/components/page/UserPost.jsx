0f1d3cfee8db7ebb6e6d74c60691ca93863cf293
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-16 11:04:23 +0100

    Add: initial UserPost() component

---
+++ src/components/page/UserPost.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import tw from 'tailwind-styled-components';
+
+export const UserPost = () => (
+  <div className="bg-black/[.2] rounded-lg p-6">
+    <p>
+      This post page is for the user <code>hello</code> on the DRF db.
+    </p>
+  </div>
+);
