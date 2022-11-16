6e36f373fb675006b217846bb0b589dc10a938a2
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-16 03:04:37 +0100

    Refactor: static button text to dynamic

--- src/components/form/input/ButtonLabelInput.jsx
+++ src/components/form/input/ButtonLabelInput.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { ImageInput } from './ImageInput';
 import { handleKeypress } from '../../../utils/handleKeypress';
 
-export const ButtonLabelInput = ({ id, svg, onChange }) => (
+export const ButtonLabelInput = ({ id, svg, text, onChange }) => (
   <>
     <button type="button" onKeyDown={handleKeypress}>
       <label htmlFor={id} className="flex items-center gap-2 cursor-pointer">
         {svg}
-        {` `}Change
+        {` `}
+        {text}
       </label>
     </button>
     <ImageInput id={id} onChange={onChange} />
