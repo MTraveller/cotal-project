9332aa9b9269201084928f6b702eaac19176840c
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-16 12:56:30 +0100

    Add: TwoSectionRight() template

--- src/pages/in/[slug].jsx
+++ src/pages/in/[slug].jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const User = () => {
-  return (
-    <div>
-      <h1>User Page</h1>
-    </div>
-  );
-};
+import { TwoSectionRight } from '../../components/layout/template/private-route/TwoSectionRight';
+import { UserPageDetail } from '../../components/page/user-page/UserPageDetail';
+import { SideBar } from '../../components/page/side-bar/two-section/SideBar';
+import Seo from '../../components/Seo';
 
-export default User;
+const UserPage = () => (
+  <TwoSectionRight
+    leftStyles="flex-auto w-[460px] lg:max-w-[740px]"
+    rightStyles="flex-initial w-full mx-auto sm:w-80"
+    componentLeft={UserPageDetail}
+    componentRight={SideBar}
+  />
+);
+
+export const Head = () => <Seo title="User Page" />;
+
+export default UserPage;
