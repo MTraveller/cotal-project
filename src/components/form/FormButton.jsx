ad225d2c5aded35ecb230c73410c3306e629ae39
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-17 22:00:32 +0100

    Refactor: move styles to re-usable component

--- src/components/form/FormButton.jsx
+++ src/components/form/FormButton.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import { ButtonStyles } from '../layout/style/ButtonStyle';
+
 export const FormButton = ({ handleSubmit, buttonText }) => (
-  <button
+  <ButtonStyles
     type="submit"
     data-message="Save changes made"
-    className="w-full mt-3 py-2 dark:bg-slate-900 rounded-md ring-1 dark:ring-slate-400 transition ease-in-out delay-150 hover:ring-1 hover:dark:ring-yellow-400 hover:ring-offset-4 hover:ring-offset-gray-900"
     onClick={handleSubmit}
   >
     {buttonText}
-  </button>
+  </ButtonStyles>
 );
