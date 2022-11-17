d1c44b549159ee04a73380726e308616df33366a
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-17 22:05:27 +0100

    Add: prop to pass on styles to be applied

--- src/components/layout/element/OpenLinkExternal.jsx
+++ src/components/layout/element/OpenLinkExternal.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export const OpenLinkExternal = ({ url, buttonText }) => (
+export const OpenLinkExternal = ({ className, url, buttonText }) => (
   <button
     type="button"
+    className={className}
     onClick={() => window.open(url, `_blank`, `noopener noreferrer`)}
   >
     {buttonText}
