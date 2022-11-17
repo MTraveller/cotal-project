adb7fc775453996da3beb60f04c71ca1f340c8ab
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-17 22:09:41 +0100

    Add: buttons to render either requests or network

--- src/components/page/user-network/NetworkMenu.jsx
+++ src/components/page/user-network/NetworkMenu.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
-// import tw from 'tailwind-styled-components';
 
-export const NetworkMenu = () => (
-  <div className="bg-black/[.2] rounded-lg p-6">
-    <p>Hello from Network Menu</p>
-  </div>
-);
+import { ButtonStyles } from '../../layout/style/ButtonStyle';
+
+export const NetworkMenu = ({ setMenu }) => {
+  const handleClick = ({ target: button }) => {
+    setMenu(button.id);
+  };
+
+  return (
+    <div className="bg-black/[.2] rounded-lg p-6">
+      <div className="flex flex-row lg:flex-col gap-5">
+        <ButtonStyles id="request" type="button" onClick={handleClick}>
+          View Requests
+        </ButtonStyles>
+        <ButtonStyles id="network" type="button" onClick={handleClick}>
+          View Network
+        </ButtonStyles>
+      </div>
+    </div>
+  );
+};
