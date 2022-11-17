d6e92af921461664158512dfaca054e4bf07207e
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-17 22:05:56 +0100

    Add: initial ButtonStyles component

---
+++ src/components/layout/style/ButtonStyle.jsx
@@ -0,0 +1,18 @@
+import tw from 'tailwind-styled-components';
+
+export const ButtonStyles = tw.button`
+  w-full
+  py-2
+  dark:bg-slate-900
+  rounded-md
+  ring-1
+  dark:ring-slate-400
+  transition
+  ease-in-out
+  hover:ring-1
+  hover:ring-offset-4
+  hover:ring-offset-gray-900
+  hover:dark:ring-yellow-400
+  active:ring-offset-1
+  active:dark:ring-pink-400
+`;
