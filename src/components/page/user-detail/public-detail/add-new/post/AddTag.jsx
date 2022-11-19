e6fa5927678e56dab0cb29f8288b39af673c4f2b
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-19 07:00:40 +0100

    Add: AddTag() component to add additional tags inputs

---
+++ src/components/page/user-detail/public-detail/add-new/post/AddTag.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const AddTag = ({ handleClick }) => {
+  return (
+    <div className="w-1/6 flex flex-row justify-around">
+      <button
+        id="remove-tag"
+        type="button"
+        className="w-10 h-10 rounded-full dark:bg-pink-900 hover:dark:bg-pink-800 dark:text-slate-400 text-center"
+        onClick={handleClick}
+      >
+        <span className="font-bold">-1</span>
+      </button>
+      <button
+        id="add-tag"
+        type="button"
+        className="w-10 h-10 rounded-full dark:bg-lime-900 hover:dark:bg-lime-800 dark:text-slate-400 text-center"
+        onClick={handleClick}
+      >
+        <span className="font-bold">+1</span>
+      </button>
+    </div>
+  );
+};
