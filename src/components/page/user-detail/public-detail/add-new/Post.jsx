b3921329b93608f9b2f1110be8ab9eaccf9d42c9
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-19 07:02:32 +0100

    Add: Post() component to add new posts

---
+++ src/components/page/user-detail/public-detail/add-new/Post.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+import { Input } from '../../../../form/input/Input';
+import { TagInput } from '../../../../form/input/TagInput';
+import { Textarea } from '../../../../form/input/Textarea';
+import { AddTag } from './post/AddTag';
+
+export const Post = ({ form, handleChange }) => {
+  const [tagCount, setTagCount] = useState([1]);
+
+  const handleClick = ({ currentTarget: button }) => {
+    if (button.id === `add-tag`) {
+      setTagCount([...tagCount, tagCount.length + 1]);
+    } else if (button.id === `remove-tag`) {
+      const newCount = [...tagCount];
+      newCount.pop();
+      setTagCount(newCount);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <label htmlFor="title" className="sr-only">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          type="title"
+          value={form.title}
+          autoComplete="off"
+          inputDisplay="block"
+          borderRadius="rounded-none rounded-t-md"
+          padding="px-3 py-3 md:py-4"
+          placeholder="Title"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-5">
+        <label htmlFor="link" className="sr-only">
+          Link
+        </label>
+        <Input
+          id="link"
+          name="link"
+          type="link"
+          value={form.link}
+          autoComplete="off"
+          inputDisplay="block"
+          borderRadius="rounded-none rounded-b-md"
+          padding="px-3 py-3 md:py-4"
+          placeholder="Link?"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="description" className="sr-only">
+          Description
+        </label>
+        <Textarea
+          id="description"
+          name="description"
+          type="description"
+          value={form.description}
+          autoComplete="off"
+          inputDisplay="block"
+          borderRadius="rounded-md"
+          padding="px-3 py-3 md:py-4"
+          placeholder="Description .."
+          onChange={handleChange}
+        />
+        <div className="w-full flex flex-wrap items-center gap-y-4 gap-x-3 my-5">
+          {tagCount.map((idx) => (
+            <TagInput key={idx} onChange={handleChange} />
+          ))}
+          <AddTag handleClick={handleClick} />
+        </div>
+      </div>
+    </>
+  );
+};
