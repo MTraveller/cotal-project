db67b8f427283d3f06e45cb96b0392ca4163927a
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-19 23:27:16 +0100

    Add: space between 3rd part imports and components import

--- src/components/form/input/ButtonLabelInput.jsx
+++ src/components/form/input/ButtonLabelInput.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { ImageInput } from './ImageInput';
 import { handleKeypress } from '../../../utils/handleKeypress';
 
--- src/components/form/input/profile/StatusInput.jsx
+++ src/components/form/input/profile/StatusInput.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react';
+
 import { Listbox, Transition } from '@headlessui/react';
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
