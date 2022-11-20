21bfa4fc9ba40f78ee3530e477ca3e95ef6d49b9
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-20 19:50:57 +0100

    Add: <Loader /> instead of directly added svg

--- src/components/form/indexButton.jsx
+++ src/components/form/indexButton.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Loader from '../layout/element/loader';
+
 export const SubmitButton = ({ buttonText }) => (
   <button
     type="submit"
@@ -23,21 +25,7 @@ export const SubmitButton = ({ buttonText }) => (
     <span>
       <span id="button-loading" className="hidden">
         <span className="flex flex-row italic">
-          <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
+          <Loader styles="h-5 w-5 mr-3" />
           Processing...
         </span>
       </span>
