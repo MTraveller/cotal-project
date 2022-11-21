620dfab67db28df974fdf8f62125eb1691fd5980
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-21 16:08:05 +0100

    Add: sticky class

--- src/components/page/side-bar/three-section/SideBarRight.jsx
+++ src/components/page/side-bar/three-section/SideBarRight.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const SideBarRight = () => {
   return (
-    <div className="bg-black/[.2] rounded-lg p-6">
+    <div className="bg-black/[.2] lg:sticky lg:top-[80px] rounded-lg p-6">
       <p>Tags from SidebarRight</p>
     </div>
   );
