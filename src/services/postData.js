d4082ee30dee9304dbe5fa1a4e6e42a41491b45b
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-22 16:03:35 +0100

    Add: processes for posting data to backend depending on endpoint

--- src/services/postData.js
+++ src/services/postData.js
@@ -6,16 +6,48 @@ import {
 } from './userPostHandler';
 
 export default async function postDataHandler(req) {
+  let error = [];
+
   if (req[0] === `personal`) {
-    const profileLength = Object.keys(req[2]).length;
+    if (req[2] !== undefined) {
+      if (req[2].socials) {
+        const response = await userSocialHandler({
+          user: req[1],
+          data: req[2].socials,
+          token: req[3],
+        });
 
-    if (req[2].socials) await userSocialHandler({ data: req[2].socials });
-    if (req[2].linktrees)
-      await userLinktreesHandler({ data: req[2].linktrees });
+        if (response !== true) error = [...response];
+      }
 
-    delete req[2].socials;
-    delete req[2].linktrees;
+      if (req[2].linktrees) {
+        const response = await userLinktreesHandler({
+          user: req[1],
+          data: req[2].linktrees,
+          token: req[3],
+        });
 
-    if (profileLength > 0) await userProfileHandler({ data: req[2] });
-  } else if (req[0] === `connect`) await userConnectHandler({ data: req[1] });
+        if (response !== true) error = [...response];
+      }
+
+      delete req[2].socials;
+      delete req[2].linktrees;
+
+      if (Object.keys(req[2]).length > 0) {
+        const response = await userProfileHandler({
+          data: req[2],
+          token: req[3],
+        });
+
+        if (response !== true) error = [...response];
+      }
+    }
+  } else if (req[0] === `connect`) {
+    const response = await userConnectHandler({ data: req[1], token: req[3] });
+
+    if (response !== true) error = [...response];
+  }
+
+  if (error.length !== 0) return error;
+  return true;
 }
