d156689ec246bf2d893514e5610d5e94f4f97e1d
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-22 16:02:14 +0100

    Add: setUserData to UserDataContext to re-render when needed

--- src/context/UserDataContext.jsx
+++ src/context/UserDataContext.jsx
@@ -5,23 +5,25 @@ import getUserDatahandler from '../services/userData';
 
 export const UserDataContext = createContext({
   userData: {},
+  setUserData: () => {},
 });
 
 export const UserDataProvider = ({ children }) => {
   const [userData, setUserData] = useState();
 
   useEffect(() => {
-    getUserDatahandler(`/profiles/me/`, getUser().access)
-      .then((res) => {
-        return setUserData(res.data);
-      })
-      .catch((ex) => {
-        return ex;
-      });
-  }, []);
+    if (!userData)
+      getUserDatahandler(`/profiles/me/`, getUser().access)
+        .then((res) => {
+          return setUserData(res.data);
+        })
+        .catch((ex) => {
+          return ex;
+        });
+  }, [userData]);
 
   return (
-    <UserDataContext.Provider value={{ userData }}>
+    <UserDataContext.Provider value={{ userData, setUserData }}>
       {children}
     </UserDataContext.Provider>
   );
