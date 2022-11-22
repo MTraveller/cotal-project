fd6a8478637fbcd8525a1f053f071daa5dbe5387
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-22 16:07:14 +0100

    Refactor: code to apply to latest changes to effectively check if items has changed

--- src/utils/checkEquality.js
+++ src/utils/checkEquality.js
@@ -1,37 +1,28 @@
-export const checkEquality = (state, server) => {
-  const stateEntries = Object.entries(state);
+export const checkEquality = (personal, social, server) => {
+  const personalEntries = Object.entries(personal);
+  const socialsEntries = Object.entries(social);
   const serverEntries = Object.entries(server);
 
   const serverChanges = {};
 
-  stateEntries.forEach((obj, idx) => {
+  personalEntries.forEach((obj, idx) => {
     if (typeof obj[1] === `object`) {
       if (obj[1] !== null) {
         if (obj[0] === `image`) {
           if (serverEntries[idx][1]?.name !== obj[1].name) {
             serverChanges[obj[0]] = obj[1];
           }
-        } else {
-          if (obj[0] === `socials`) {
-            const socialServerChanges = [];
-
-            Object.entries(obj[1]).forEach((o, i) => {
-              if (serverEntries[idx][1][i].username !== o[1].username) {
-                socialServerChanges.push(stateEntries[idx][1][i]);
-              }
-            });
-
-            serverChanges[obj[0]] = socialServerChanges;
-          }
         }
-
-        console.log('isArray', idx, Array.isArray(obj));
       }
     } else {
       if (serverEntries[idx][1] !== obj[1]) {
-        if (stateEntries[idx][1] !== undefined) {
-          if (stateEntries[idx][0] === `linktrees`) {
-            serverChanges[obj[0]] = [{ username: obj[1] }];
+        if (personalEntries[idx][1] !== undefined) {
+          if (personalEntries[idx][0] === `linktrees`) {
+            if (serverEntries[idx][1][0].username !== obj[1]) {
+              serverChanges[obj[0]] = [
+                { id: serverEntries[idx][1][0].id, username: obj[1] },
+              ];
+            }
           } else {
             serverChanges[obj[0]] = obj[1];
           }
@@ -39,6 +30,25 @@ export const checkEquality = (state, server) => {
       }
     }
   });
+
+  serverChanges.socials = {};
+  socialsEntries.forEach((o, idx) => {
+    if (serverEntries[4][1][idx]?.username !== o[1].username) {
+      if (serverEntries[4][1][idx]?.id) {
+        serverChanges[`socials`][o[0]] = {
+          id: serverEntries[4][1][idx].id,
+          username: o[1].username,
+        };
+      } else {
+        serverChanges[`socials`][o[0]] = o[1];
+      }
+    }
+  });
+
+  const socialsLength = { ...serverChanges.socials };
+  if (Object.keys(socialsLength).length === 0) delete serverChanges[`socials`];
+
+  if (Object.keys(serverChanges).length === 0) return null;
 
   return serverChanges;
 };
