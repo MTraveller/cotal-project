e2214dd958fb41e80b5c33e2f0c3f893280ca9fc
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-23 22:29:08 +0100

    Add: support for both add new and edit post in a single unified component

--- src/components/page/user-detail/public-detail/AddNewEdit.jsx
+++ src/components/page/user-detail/public-detail/AddNewEdit.jsx
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from 'react';
 
+import getUserDataHandler from '../../../../services/userData';
 import postDataHandler from '../../../../services/postData';
-import { displayLoader, getUser } from '../../../export/personalDetail';
+import {
+  checkEquality,
+  displayLoader,
+  getUser,
+} from '../../../export/personalDetail';
 import { ButtonLabelInput } from '../../../form/input/ButtonLabelInput';
 import { UploadSvg } from '../../../form/input/UploadSvg';
 import { Post } from './add-new/Post';
 import { ProfileModel } from './add-new/ProfileModel';
 import { FormButton } from '../../../form/FormButton';
+import { toast } from 'react-toastify';
 
-export const AddNew = ({ userSlug, setActive, model }) => {
+export const AddNewEdit = ({
+  userSlug,
+  setActive,
+  model,
+  object,
+  setTargetName,
+  setObject,
+}) => {
   const [form, setForm] = useState({
     image: undefined,
     title: ``,
@@ -16,9 +29,51 @@ export const AddNew = ({ userSlug, setActive, model }) => {
     link: ``,
   });
 
+  const [isObject, setIsObject] = useState(null);
+  const [objectDB, setObjectDB] = useState(null);
   const [tag, setTag] = useState([]);
-
   const [preview, setPreview] = useState();
+
+  useEffect(() => {
+    if (object) setIsObject(true);
+
+    if (isObject && !objectDB) {
+      if (userSlug) {
+        const objectModel = model.toLowerCase();
+        let trail = ``;
+        if (objectModel === `post`) {
+          trail = `/posts/profiles/${userSlug}/${objectModel}s/${object}/`;
+        } else {
+          trail = `/profiles/${userSlug}/${objectModel}s/${object}/`;
+        }
+        getUserDataHandler(trail, getUser().access)
+          .then((res) => {
+            return setObjectDB(res.data);
+          })
+          .catch((ex) => {
+            return ex;
+          });
+      }
+    }
+
+    if (objectDB) {
+      setForm({
+        id: objectDB.id,
+        image: objectDB.image,
+        title: objectDB.title,
+        [model === `Post` ? `post` : `description`]:
+          model === `Post` ? objectDB.post : objectDB.description,
+      });
+
+      setPreview(objectDB.image);
+    }
+  }, [object, isObject, userSlug, model, objectDB]);
+
+  const handleClickBack = () => {
+    setActive(false);
+    setTargetName(null);
+    setObject(null);
+  };
 
   const onSelectFile = (e) => {
     if (!e.target.files || e.target.files.length === 0) return;
@@ -34,39 +89,68 @@ export const AddNew = ({ userSlug, setActive, model }) => {
   // "add_tags": ["django"]
 
   const handleChange = ({ currentTarget: input }) => {
-    console.log(input.id);
     setForm({ ...form, [input.name]: input.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    toast.dismiss();
+
     const button = e.currentTarget;
 
-    const formCopy = { ...form };
+    let formCopy = { ...form };
 
     if (tag.length) formCopy[`tag`] = tag;
     if (formCopy.image === undefined) delete formCopy.image;
-    if (button.id === `Post`) delete formCopy.link;
+    if (button.id === `Post` || !formCopy.link) delete formCopy.link;
 
-    displayLoader(e);
-    const response = await postDataHandler([
-      model,
-      userSlug,
-      formCopy,
-      getUser().access,
-    ]);
+    formCopy[`content`] = true;
 
-    console.log(response);
-    if (response) {
+    const result = checkEquality(formCopy, objectDB);
+
+    if (result) {
       displayLoader(e);
+
+      const isPost = model === `Post` ? true : false;
+      const contentBodyName = isPost ? `post` : `description`;
+
+      if (!result.title) result[`title`] = form.title;
+      if (!result.contentBodyName)
+        result[contentBodyName] = isPost ? form.post : form.description;
+      if (object) result[`slug`] = object;
+
+      const response = await postDataHandler([
+        model,
+        userSlug,
+        result,
+        getUser().access,
+        isPost,
+        contentBodyName,
+      ]);
+
+      if (response === true) {
+        toast.success(`Saved successfully`);
+        setIsObject(null);
+        setObjectDB(null);
+      } else {
+        response.length !== 1
+          ? toast.warn(response[0])
+          : toast.warn(response[0][0]);
+      }
+      displayLoader(`${model.toLowerCase()}-submit`);
+      button.parentNode.parentNode.parentNode.firstChild.lastChild.click();
+    } else {
+      toast(`It works, but no input no output ;)`);
     }
+
+    return null;
   };
 
   return (
     <div className="flex flex-col gap-y-6">
       <div className="flex flex-row justify-between items-center">
         <h2>Add {model}</h2>
-        <button type="button" onClick={() => setActive(false)}>
+        <button type="button" onClick={handleClickBack}>
           Back
         </button>
       </div>
@@ -116,7 +200,7 @@ export const AddNew = ({ userSlug, setActive, model }) => {
             />
           )}
           <FormButton
-            id={model}
+            id={`${model.toLowerCase()}-submit`}
             extraStyles="mt-5 py-2"
             handleSubmit={handleSubmit}
             buttonText="Submit"
