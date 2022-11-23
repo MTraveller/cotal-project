0e69e2d0275f4ba6dbbc2db916bbe3dad87848f8
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-23 22:40:33 +0100

    Add: portfolio dynamic page

---
+++ src/pages/in/[slug]/portfolio/[portfolio].jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { ThreeSection } from '../../../../components/layout/template/private-route/ThreeSection';
+import { SideBarLeft } from '../../../../components/page/side-bar/three-section/SideBarLeft';
+import { UserPost } from '../../../../components/page/UserPost';
+import { SideBarRight } from '../../../../components/page/side-bar/three-section/SideBarRight';
+
+export default function Post({ params }) {
+  return (
+    <ThreeSection
+      leftStyles="hidden lg:block flex-1 max-w-60"
+      middleStyles="flex-auto w-[500px] max-w-[782px]"
+      rightStyles="hidden lg:block flex-1 max-w-60"
+      componentLeft={SideBarLeft}
+      componentMiddle={UserPost}
+      componentRight={SideBarRight}
+      params={params}
+      model="portfolio"
+    />
+  );
+}
