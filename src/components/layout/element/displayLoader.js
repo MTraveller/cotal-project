a2f90c7e37a0c943f57bde240fefb3f50ee8fc5c
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-23 02:48:33 +0100

    Add: code to handle removing the loading state of the button

--- src/components/layout/element/displayLoader.js
+++ src/components/layout/element/displayLoader.js
@@ -1,5 +1,21 @@
-export const displayLoader = ({ currentTarget: button }) => {
-  button.setAttribute(`disabled`, ``);
+export const displayLoader = (e) => {
+  let button = e.currentTarget;
+  if (!button) button = e.target;
+
+  console.log(button);
+  let disabled = false;
+  console.log(disabled);
+
+  Array.from(button.attributes).forEach((attr) => {
+    if (attr.name === `disabled`) disabled = true;
+  });
+
+  console.log(disabled);
+
+  disabled
+    ? button.removeAttribute(`disabled`)
+    : button.setAttribute(`disabled`, ``);
+
   button.childNodes[0].classList.toggle(`hidden`);
   button.childNodes[1].classList.toggle(`hidden`);
 };
