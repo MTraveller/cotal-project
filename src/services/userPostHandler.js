b77c5decb31fd7fe546871b33af65644c13cd265
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-23 22:42:27 +0100

    Add: post content handler to handle all content post and put request to backend

--- src/services/userPostHandler.js
+++ src/services/userPostHandler.js
@@ -75,9 +75,6 @@ export async function userSocialHandler({ user, data, token }) {
 }
 
 export async function userLinktreesHandler({ user, data, token }) {
-  console.log(user);
-  console.log(data);
-  console.log(data[0].username);
   const headers = {
     Authorization: `Cotal ${token}`,
   };
@@ -117,4 +114,72 @@ export async function userLinktreesHandler({ user, data, token }) {
 
 export async function userConnectHandler({ user, data, token }) {
   console.log(data);
+}
+
+export async function userPostContentHander({
+  isPost,
+  type,
+  model,
+  user,
+  data,
+  token,
+}) {
+  const modelName = model.toLowerCase();
+
+  const formData = new FormData();
+
+  if (data.image) formData.append(`image`, data.image);
+  if (data.title) formData.append(`title`, data.title);
+  if (isPost ? data.post : data.description)
+    formData.append(type, isPost ? data.post : data.description);
+  if (data.link) formData.append(`link`, data.link);
+
+  const headers = {
+    Authorization: `Cotal ${token}`,
+  };
+
+  const error = [];
+
+  const res = data.slug
+    ? await http
+        .put(
+          isPost
+            ? `${apiEndpoint}/posts/profiles/${user}/posts/${data.slug}/`
+            : `${apiEndpoint}/profiles/${user}/${modelName}s/${data.slug}/`,
+          formData,
+          {
+            headers,
+          }
+        )
+        .then((res) => res)
+        .catch((ex) => ex)
+    : await http
+        .post(
+          isPost
+            ? `${apiEndpoint}/posts/profiles/${user}/posts/`
+            : `${apiEndpoint}/profiles/${user}/${modelName}s/`,
+          formData,
+          {
+            headers,
+          }
+        )
+        .then((res) => res)
+        .catch((ex) => ex);
+
+  if (res.status === 200 || res.status === 201) {
+    return true;
+  } else if (res.code === `ERR_NETWORK`) {
+    error.push(`Cotal Backend`, res.code);
+  } else if (res.status >= 400 && res.status < 500) {
+    console.log(res.response);
+    Object.entries(res.data).forEach((arr) => {
+      error.push(arr[1]);
+    });
+  }
+
+  console.log(res.response.status >= 400 && res.response.status < 500);
+
+  console.log(error);
+  if (error.length !== 0) return error;
+  return true;
 }
