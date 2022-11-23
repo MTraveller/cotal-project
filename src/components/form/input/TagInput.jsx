98e48ce6e58813d879ed846be7b41fdabe277a3d
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-23 02:47:34 +0100

    Add: handleBlur() to save tags to parent AddNew tag state

--- src/components/form/input/TagInput.jsx
+++ src/components/form/input/TagInput.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import { Input } from './Input';
 
-export const TagInput = () => {
+export const TagInput = ({ tagArray, setTag }) => {
   const [tagId, setTagId] = useState();
   const [value, setValue] = useState();
 
@@ -11,8 +11,34 @@ export const TagInput = () => {
   }, [tagId]);
 
   const handleChange = ({ currentTarget: input }) => {
-    console.log(input.value);
     setValue(input.value);
+  };
+
+  const handleBlur = ({ currentTarget: input }) => {
+    if (input.value.length) {
+      let idx = 0;
+      const isTag = tagArray.filter((tag, i) => {
+        idx = i;
+        return tag[0] === input.id;
+      });
+
+      console.log(isTag);
+      if (!isTag || isTag.length === 0) {
+        setTag([...tagArray, [input.id, input.value]]);
+      } else {
+        const isSame = isTag[1] === input.value ? true : false;
+
+        if (!isSame) {
+          const array = [...tagArray];
+          const item = array.pop(idx);
+
+          item.splice(1, 1, input.value);
+          array.push(item);
+
+          setTag(array);
+        }
+      }
+    }
   };
 
   return (
@@ -22,14 +48,15 @@ export const TagInput = () => {
       type="tag"
       value={value}
       autoComplete="off"
-      width="w-1/6"
-      padding="px-3 py-2 md:py-2"
+      width="flex-auto w-[175px] sm:basis-2/6"
+      padding="px-3 py-2"
       borderRadius="rounded-md"
       bgStyles="dark:bg-slate-800/40 dark:border-slate-800/70"
       textColor="text-slate-500"
       placeHolderColor="placeholder-slate-500"
       placeholder="Add tag.."
       onChange={handleChange}
+      onBlur={handleBlur}
     />
   );
 };
