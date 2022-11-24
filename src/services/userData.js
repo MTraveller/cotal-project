bf19a86491c63fdd8976dfad194b10e5b9191d8b
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-24 23:25:37 +0100

    Add: initial allUserDataHandler() to handle all user information from backend to be displayed on UserPageDetail()

--- src/services/userData.js
+++ src/services/userData.js
@@ -25,3 +25,36 @@ export default async function getUserDataHandler(urlTrail, token) {
 
   return result;
 }
+
+export async function allUserDataHandler(user) {
+  // GET all user data from api
+  const data = {};
+
+  const profile = await http
+    .get(apiProfileMeEndpoint + `/profiles/` + user + `/`, {})
+    .then((res) => res)
+    .catch((ex) => ex);
+
+  if (profile.status !== 200) data[`profile`] = `error`;
+  data[`profile`] = profile.data;
+
+  const post = await http
+    .get(apiProfileMeEndpoint + `/posts/profiles/` + user + `/posts/`, {})
+    .then((res) => res)
+    .catch((ex) => ex);
+
+  if (profile.status !== 200) data[`profile`] = `error`;
+  data[`post`] = post.data;
+
+  for await (const model of [`portfolio`, `award`, `certificate`, `creative`]) {
+    const res = await http
+      .get(apiProfileMeEndpoint + `/profiles/` + user + `/` + model + `s/`, {})
+      .then((res) => res)
+      .catch((ex) => ex);
+
+    if (res.status !== 200) data[model] = `error`;
+    data[model] = res.data;
+  }
+
+  return data;
+}
