e070aabf3a95b25eaef6bf4c0be715e19a7c2b63
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-24 23:21:36 +0100

    Add: initial less than three items component that adapts to less than 2 items

---
+++ src/components/page/user-page/grid/LessThanTwo.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import { Image } from '../../../layout/element/Image';
+
+export const LessThanTwo = ({ data, model, user, colorTop, colorBottom }) => (
+  <section className="w-full mb-6">
+    <h2 className="mb-8 text-2xl text-stone-600/80">
+      {`${model}s`.toUpperCase()}
+    </h2>
+    {data[model].map((obj) => {
+      return (
+        <Link
+          key={obj.title}
+          to={`/in/${user}/${model}/${obj.slug}/`}
+          alt={obj.title}
+        >
+          <h2
+            className={`px-2 bt-2 pb-0 ${colorTop} rounded-none rounded-t-md`}
+          >
+            {obj.title.length > 24
+              ? `${obj.title.substring(0, 24)}...`
+              : obj.title}
+          </h2>
+          {obj.image ? (
+            <figure>
+              <Image
+                image={obj.image}
+                addedModelName={model}
+                slug={user}
+                alt={obj.title}
+              />
+            </figure>
+          ) : (
+            ``
+          )}
+          <span
+            className={`inline-block w-full px-2 pt-1 pb-1 text-xs ${colorBottom} rounded-none rounded-b-md`}
+          >
+            {new Date(obj.created_on).toDateString()}
+          </span>
+        </Link>
+      );
+    })}
+  </section>
+);
