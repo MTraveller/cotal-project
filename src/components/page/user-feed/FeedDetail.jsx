079100481f11e8fe42d1dc0a21f4e83f0ee670df
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-24 23:16:58 +0100

    Add: return seo title from within function

--- src/components/page/user-feed/FeedDetail.jsx
+++ src/components/page/user-feed/FeedDetail.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-// import tw from 'tailwind-styled-components';
+
+import Seo from '../../Seo';
 
 export const FeedDetail = () => (
-  <div className="bg-black/[.2] rounded-lg p-6">
-    <p>
-      This feed page is for the user <code>hello</code> on the DRF db.
-    </p>
-  </div>
+  <>
+    <Seo title="Feed" />
+
+    <div className="bg-black/[.2] rounded-lg p-6">
+      <p>
+        This feed page is for the user <code>hello</code> on the DRF db.
+      </p>
+    </div>
+  </>
 );
