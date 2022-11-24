31307e1d12f4c91d9c12ada4a3ff250f60371cfe
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-24 23:13:54 +0100

    Add: reason for not working

--- src/components/page/UserNetwork.jsx
+++ src/components/page/UserNetwork.jsx
@@ -9,14 +9,23 @@ const UserNetwork = () => {
   const [menu, setMenu] = useState('request');
 
   return (
-    <TwoSectionLeft
-      leftStyles="flex-initial w-full mx-auto sm:w-80"
-      rightStyles="flex-auto w-[460px] lg:max-w-[740px]"
-      componentLeft={NetworkMenu}
-      componentRight={NetworkDetail}
-      menu={menu}
-      setMenu={setMenu}
-    />
+    <>
+      <Seo title={menu} />
+
+      <p className="text-stone-600/70 text-xs text-center">
+        Due to deadline approacking this feature is not handling http.post
+        request to the backend, with more time, sure would be functional.
+      </p>
+
+      <TwoSectionLeft
+        leftStyles="flex-initial w-full mx-auto sm:w-80"
+        rightStyles="flex-auto w-[460px] lg:max-w-[740px]"
+        componentLeft={NetworkMenu}
+        componentRight={NetworkDetail}
+        menu={menu}
+        setMenu={setMenu}
+      />
+    </>
   );
 };
 
