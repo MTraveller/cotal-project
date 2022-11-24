7a288ee4f8d56956455aee86577f51df87569d5e
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-24 23:14:48 +0100

    Add: reason for not working and add seo title

--- src/components/page/UserSetting.jsx
+++ src/components/page/UserSetting.jsx
@@ -4,12 +4,22 @@ import { OneSection } from '../layout/template/private-route/OneSection';
 import { PersonalSetting } from './user-setting/PersonalSetting';
 import Seo from '../Seo';
 
+// Future feature due to deadline approaching
 const UserSetting = ({ userData }) => (
-  <OneSection
-    title="Your Settings"
-    userData={userData}
-    component={PersonalSetting}
-  />
+  <>
+    <Seo title="Settings" />
+
+    <p className="mb-4 text-stone-600/70 text-xs text-center">
+      Due to deadline approacking this feature is not set up, with more time,
+      sure would be functional.
+    </p>
+
+    <OneSection
+      title="Settings"
+      userData={userData}
+      component={PersonalSetting}
+    />
+  </>
 );
 
 export const Head = () => <Seo title="User Settings" />;
