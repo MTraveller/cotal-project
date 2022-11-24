15eda5027808f9c8597acac585bfa660c57955f2
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-24 13:43:02 +0100

    Add: context to sidebar for future features

--- src/components/page/side-bar/three-section/SideBarLeft.jsx
+++ src/components/page/side-bar/three-section/SideBarLeft.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const SideBarLeft = () => {
   return (
     <div className="bg-black/[.2] lg:sticky lg:top-[80px] rounded-lg p-6">
-      <p>Tags from Sidebarleft</p>
+      <p className="opacity-70">Some buttons here</p>
     </div>
   );
 };
