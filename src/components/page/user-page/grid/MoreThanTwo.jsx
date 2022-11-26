80b66dc55a58da3e110d0f6b6ae2c3189514c71d
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-26 03:01:52 +0100

    Add: condition if no posts return

--- src/components/page/user-page/grid/MoreThanTwo.jsx
+++ src/components/page/user-page/grid/MoreThanTwo.jsx
@@ -22,44 +22,50 @@ const GridStyles = styled.section`
 
 export const MoreThanTwo = ({ data, model, user, colorTop, colorBottom }) => (
   <>
-    <h2 className="-mb-6 text-2xl text-stone-600/80">
-      {`${model}s`.toUpperCase()}
-    </h2>
-    <GridStyles>
-      {data[model].map((obj) => {
-        return (
-          <Link
-            key={obj.title}
-            to={`/in/${user}/${model}/${obj.slug}/`}
-            alt={obj.title}
-          >
-            <h2
-              className={`px-2 bt-2 pb-0 ${colorTop} rounded-none rounded-t-md`}
-            >
-              {obj.title.length > 24
-                ? `${obj.title.substring(0, 24)}...`
-                : obj.title}
-            </h2>
-            {obj.image ? (
-              <figure>
-                <Image
-                  image={obj.image}
-                  addedModelName={model}
-                  slug={user}
-                  alt={obj.title}
-                />
-              </figure>
-            ) : (
-              ``
-            )}
-            <span
-              className={`inline-block w-full px-2 pt-1 pb-1 text-xs ${colorBottom} rounded-none rounded-b-md`}
-            >
-              {new Date(obj.created_on).toDateString()}
-            </span>
-          </Link>
-        );
-      })}
-    </GridStyles>
+    {data[model].length > 0 ? (
+      <>
+        <h2 className="-mb-6 text-2xl text-stone-600/80 capitalize">
+          {`${model}s`.toUpperCase()}
+        </h2>
+        <GridStyles>
+          {data[model].map((obj) => {
+            return (
+              <Link
+                key={obj.title}
+                to={`/in/${user}/${model}/${obj.slug}/`}
+                alt={obj.title}
+              >
+                <h2
+                  className={`px-2 bt-2 pb-0 ${colorTop} rounded-none rounded-t-md`}
+                >
+                  {obj.title.length > 24
+                    ? `${obj.title.substring(0, 24)}...`
+                    : obj.title}
+                </h2>
+                {obj.image ? (
+                  <figure>
+                    <Image
+                      image={obj.image}
+                      addedModelName={model}
+                      slug={user}
+                      alt={obj.title}
+                    />
+                  </figure>
+                ) : (
+                  ``
+                )}
+                <span
+                  className={`inline-block w-full px-2 pt-1 pb-1 text-xs ${colorBottom} rounded-none rounded-b-md`}
+                >
+                  {new Date(obj.created_on).toDateString()}
+                </span>
+              </Link>
+            );
+          })}
+        </GridStyles>
+      </>
+    ) : (
+      ``
+    )}
   </>
 );
