c3e62eac77232185cc12a5d3a781b98fa9dbb856
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-26 02:48:02 +0100

    Add: global colors and font size

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,6 +4,9 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` });
+exports.onRenderBody = ({ setHtmlAttributes, setBodyAttributes }) => {
+  setHtmlAttributes({ lang: `en`, className: `h-full` });
+  setBodyAttributes({
+    className: `h-full bg-gray-300 dark:bg-slate-900 text-gray-600 dark:text-slate-400 font-normal dark:font-light`,
+  });
 };
