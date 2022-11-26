66a48243160808bce1ee5c49f7fd2ee8986fdae7
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-26 17:52:59 +0100

    Remove: removeLoader() if response true

--- src/components/page/user-detail/PersonalDetail.jsx
+++ src/components/page/user-detail/PersonalDetail.jsx
@@ -133,7 +133,6 @@ export const PersonalDetail = () => {
 
       if (response === true) {
         toast.success(`Saved successfully`);
-        removeLoader(e.target);
         handleDivClick();
 
         setTimeout(() => {
