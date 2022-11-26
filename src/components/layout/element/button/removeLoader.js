fe47b82f76c6b4b683a093dc5cecb6f377e383d7
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-27 00:46:51 +0100

    Revert: back the file to its initial state

--- src/components/layout/element/button/removeLoader.js
+++ src/components/layout/element/button/removeLoader.js
@@ -1,12 +1,6 @@
 export const removeLoader = (...button) => {
-  if (button[0].id === `form-submit`) {
-    button[0].removeAttribute(`disabled`, ``);
-    button[0].firstChild.classList.toggle(`hidden`);
-    button[0].lastChild.classList.toggle(`hidden`);
-  } else {
-    button[0].removeAttribute(`disabled`, ``);
-    button[0].firstChild.classList.toggle(`hidden`);
-    button[1].lastChild.classList.toggle(`hidden`);
-    button[1].firstChild.classList.toggle(`hidden`);
-  }
+  button[0].removeAttribute(`disabled`, ``);
+  button[0].firstChild.classList.toggle(`hidden`);
+  button[1].lastChild.classList.toggle(`hidden`);
+  button[1].firstChild.classList.toggle(`hidden`);
 };
