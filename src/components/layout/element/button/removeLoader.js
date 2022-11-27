4669baa150aada933dda8adfd113023395cb25f4
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-27 01:52:58 +0100

    Add: a manual trigger to remove loader from button

--- src/components/layout/element/button/removeLoader.js
+++ src/components/layout/element/button/removeLoader.js
@@ -1,6 +1,16 @@
 export const removeLoader = (...button) => {
-  button[0].removeAttribute(`disabled`, ``);
-  button[0].firstChild.classList.toggle(`hidden`);
-  button[1].lastChild.classList.toggle(`hidden`);
-  button[1].firstChild.classList.toggle(`hidden`);
+  if (button[0].id === `form-submit`) {
+    button[0].removeAttribute(`disabled`, ``);
+    button[0].classList.toggle(`hidden`);
+    button[0].classList.toggle(`hidden`);
+  } else if (button[0] === `trigger`) {
+    button[1].removeAttribute(`disabled`, ``);
+    button[1].firstChild.classList.toggle(`hidden`);
+    button[1].lastChild.classList.toggle(`hidden`);
+  } else {
+    button[0].removeAttribute(`disabled`, ``);
+    button[0].firstChild.classList.toggle(`hidden`);
+    button[1].lastChild.classList.toggle(`hidden`);
+    button[1].firstChild.classList.toggle(`hidden`);
+  }
 };
