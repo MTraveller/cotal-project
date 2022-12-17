9ff5dea2ae721ad0c3fa8df2f2756958b98b5ebe
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-12-17 09:37:48 +0100

    Add: exports to be used in UserPageDetail.jsx for easier management of imports

---
+++ src/export/userPageDetail.js
@@ -0,0 +1,8 @@
+export { Image } from '../components/layout/element/Image';
+export { ProfileImageSvg } from '../components/layout/element/ProfileImageSvg';
+export { socialIcon } from '../components/layout/element/socialIcons';
+export { LessThanTwo } from '../components/page/user-page/grid/LessThanTwo';
+export { LessThanThree } from '../components/page/user-page/grid/LessThanThree';
+export { MoreThanTwo } from '../components/page/user-page/grid/MoreThanTwo';
+export { allUserDataHandler } from '../services/userData';
+export { ServerError } from '../components/ServerError';
